3f9755b94cbc903251f51b1a5d9edbf56e94a9a7
chaos_pantanal <chaos_pantanal@edda2279-5339-4143-a38b-a3a7c4ae352d>
2002-05-24 16:51:54 +0000

    *** empty log message ***

--- UnBBayes/src/unbbayes/datamining/gui/metaphor/MetaphorTree.java
+++ UnBBayes/src/unbbayes/datamining/gui/metaphor/MetaphorTree.java
@@ -3,6 +3,8 @@ package unbbayes.datamining.gui.metaphor;
 import java.util.*;
 import javax.swing.*;
 import javax.swing.tree.*;
+
+import sun.security.provider.JavaKeyStore;
 import unbbayes.jprs.jbn.*;
 import unbbayes.util.*;
 
@@ -10,10 +12,6 @@ import unbbayes.util.*;
  * @author Paulo F. Duarte
  */
 public class MetaphorTree extends JTree {
-    public static final int CHECK_YES = 1;
-    public static final int CHECK_NO = -1;
-    public static final int CHECK_EMPTY = 0;
-
 	private class StateObject {
 	    private int stateIndex = -1;
 	    private int check = CHECK_EMPTY;
@@ -40,10 +38,41 @@ public class MetaphorTree extends JTree {
 	    }
 	}
 
+	public class MetaphorTreeCellRenderer extends javax.swing.tree.DefaultTreeCellRenderer {
+	    ImageIcon yesIcon = new ImageIcon(getClass().getResource("/icones/yesState.gif"));
+	    ImageIcon noIcon = new ImageIcon(getClass().getResource("/icones/noState.gif"));
+	    ImageIcon emptyIcon = new ImageIcon(getClass().getResource("/icones/emptyState.gif"));
+	
+	    public java.awt.Component getTreeCellRendererComponent(JTree tree, Object value,
+	                            boolean sel, boolean expanded, boolean leaf,
+	                            int row, boolean hasFocus) {
+	
+	        super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf, row, hasFocus);
+	        DefaultMutableTreeNode treeNode = (DefaultMutableTreeNode)value;
+	        if (leaf) {
+	            Object obj = objectsMap.get(treeNode);
+	            if (obj instanceof StateObject) {
+	                StateObject stateObject = (StateObject)obj;
+	                int check = stateObject.getCheck();
+	                setIcon((check == CHECK_YES) ? yesIcon : ((check == CHECK_NO) ? noIcon : emptyIcon));
+	            }
+	            else
+	            {
+	                setIcon(getClosedIcon());
+	            }
+	        }
+	        return this;
+	    }
+	}
+
+    public static final int CHECK_YES = 1;
+    public static final int CHECK_NO = -1;
+    public static final int CHECK_EMPTY = 0;
+
 	private DefaultMutableTreeNode root = null;
 	private ProbabilisticNetwork net = null;
     private boolean showProbability = false;
-    private ArrayList rowObjectsList = new ArrayList();
+    private ArrayMap objectsMap = new ArrayMap();
 
 	public MetaphorTree() {
 		super(new DefaultMutableTreeNode(null));
@@ -51,6 +80,17 @@ public class MetaphorTree extends JTree {
         setShowsRootHandles(true);
         setSelectionModel(null);
         setRootVisible(false);
+        setCellRenderer(new MetaphorTreeCellRenderer());
+        addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                methaphorTreeMouseClicked(evt);
+            }
+        });
+	}
+
+	public MetaphorTree(boolean showProbability) {
+		this();
+		this.showProbability = showProbability;
 	}
 
 	public MetaphorTree(ProbabilisticNetwork net) {
@@ -58,25 +98,147 @@ public class MetaphorTree extends JTree {
 		setProbabilisticNetwork(net);
 	}
 
+	public MetaphorTree(ProbabilisticNetwork net, boolean showProbability) {
+		this(net);
+		this.showProbability = showProbability;
+	}
+
 	public void setProbabilisticNetwork(ProbabilisticNetwork net) {
-		if (net != null && net.equals(this.net)) {
-			this.net = net;
-			int row = 1;
-	        NodeList nos = net.getCopiaNos();
+		if (net != null) {
+			if (!net.equals(this.net)) {
+				this.net = net;
+		        root.removeAllChildren();
+		        objectsMap.clear();
+		        NodeList nos = net.getCopiaNos();
+		        for (int i = 0; i < nos.size(); i++) {
+		            Node node = (Node) nos.get(i);
+		            DefaultMutableTreeNode treeNode = new DefaultMutableTreeNode(node.getDescription());
+		            objectsMap.put(treeNode, node);
+		            for (int j = 0; j < node.getStatesSize(); j++) {
+		            	DefaultMutableTreeNode stateNode = new DefaultMutableTreeNode(node.getStateAt(j) + (showProbability ? " " + ((TreeVariable)node).getMarginalAt(j) * 100.0 + "%" : ""));
+		                treeNode.add(stateNode);
+		            	objectsMap.put(stateNode,new StateObject(j, CHECK_EMPTY));
+		            }
+		            root.add(treeNode);
+		        }
+		        ((DefaultTreeModel)getModel()).reload(root);
+			}
+		}
+		else {
+			this.net = null;
 	        root.removeAllChildren();
-	        for (int i = 0; i < nos.size(); i++) {
-	            Node node = (Node) nos.get(i);
-	            DefaultMutableTreeNode treeNode = new DefaultMutableTreeNode(node);
-	            for (int j = 0; j < node.getStatesSize(); j++) {
-	                treeNode.add(new DefaultMutableTreeNode(new StateObject(j, CHECK_EMPTY)));
-	            }
-	            root.add(treeNode);
-	        }
-	        ((DefaultTreeModel)getModel()).reload(root);
+	        objectsMap.clear();
 		}
 	}
 
 	public ProbabilisticNetwork getProbabilisticNetwork() {
 		return net;
+	}
+	
+	public void setShowProbability(boolean showProbability) {
+		if (showProbability != this.showProbability) {
+			this.showProbability = showProbability;
+			ProbabilisticNetwork temp = net;
+			setProbabilisticNetwork(null);
+			setProbabilisticNetwork(temp);
+		}
+	}
+	
+	public boolean getShowProbability() {
+		return showProbability;
+	}
+
+	public void propagate() {
+		int count = getRowCount();
+		for (int i = 0; i < count; i++) {
+			TreePath path = getPathForRow(i);
+			DefaultMutableTreeNode treeNode = (DefaultMutableTreeNode)path.getLastPathComponent();
+			Object obj = objectsMap.get(treeNode);
+			if (obj instanceof StateObject) {
+				StateObject stateObject = (StateObject)obj;
+				PotentialTable table = ((ITabledVariable)objectsMap.get(treeNode.getParent())).getPotentialTable();
+				if (stateObject.getCheck() == CHECK_YES) {
+					table.setValue(stateObject.getStateIndex(), 1);
+				}
+				else if(stateObject.getCheck() == CHECK_NO) {
+					table.setValue(stateObject.getStateIndex(), 0);
+				}
+			}
+		}
+		try {
+			net.updateEvidences();
+		}
+        catch (Exception e) {
+            System.err.print(e.getMessage());
+            e.printStackTrace();
+        }
+		if (showProbability) {
+			ProbabilisticNetwork temp = net;
+			setProbabilisticNetwork(null);
+			setProbabilisticNetwork(temp);
+		}
+	}
+
+	private void methaphorTreeMouseClicked(java.awt.event.MouseEvent evt) {
+        TreePath clickedPath = getPathForLocation(evt.getX(), evt.getY());
+        if (clickedPath != null) {
+            DefaultMutableTreeNode clickedNode = (DefaultMutableTreeNode)(clickedPath.getLastPathComponent());
+            if (clickedNode != null && clickedNode.isLeaf()) {
+                Object obj = objectsMap.get(clickedNode);
+                if (obj instanceof StateObject) {
+                    DefaultMutableTreeNode parentNode = (DefaultMutableTreeNode)clickedNode.getParent();
+                    Enumeration childrenEnum = parentNode.children();
+                    StateObject yesChecked = null;
+                    ArrayList noCheckeds = new ArrayList(),
+                              emptyCheckeds = new ArrayList();
+                    while (childrenEnum.hasMoreElements()) {
+                        DefaultMutableTreeNode child = (DefaultMutableTreeNode)childrenEnum.nextElement();
+                        if (!child.equals(clickedNode)) {
+                            if (((StateObject)objectsMap.get(child)).getCheck() == CHECK_YES) {
+                                yesChecked = (StateObject)objectsMap.get(child);
+                            }
+                            else if (((StateObject)objectsMap.get(child)).getCheck() == CHECK_NO) {
+                                noCheckeds.add(objectsMap.get(child));
+                            }
+                            else {
+                                emptyCheckeds.add(objectsMap.get(child));
+                            }
+                        }
+                    }
+                    if (SwingUtilities.isLeftMouseButton(evt)) {
+                        if (((StateObject)obj).getCheck() == CHECK_YES) {
+                            ((StateObject)obj).setCheck(CHECK_EMPTY);
+                            for (int i = 0; i < noCheckeds.size(); i++) {
+                                ((StateObject)noCheckeds.get(i)).setCheck(CHECK_EMPTY);
+                            }
+                        }
+                        else {
+                            ((StateObject)obj).setCheck(CHECK_YES);
+                            if (yesChecked != null) {
+                                yesChecked.setCheck(CHECK_NO);
+                            }
+                            for (int i = 0; i < emptyCheckeds.size(); i++) {
+                                ((StateObject)emptyCheckeds.get(i)).setCheck(CHECK_NO);
+                            }
+                        }
+                    }
+                    if (SwingUtilities.isRightMouseButton(evt)) {
+                        if (((StateObject)obj).getCheck() == CHECK_NO) {
+                            ((StateObject)obj).setCheck(CHECK_EMPTY);
+                            if (yesChecked != null) {
+                                yesChecked.setCheck(CHECK_EMPTY);
+                            }
+                        }
+                        else if (noCheckeds.size() < (parentNode.getChildCount() - 1)) {
+                            ((StateObject)obj).setCheck(CHECK_NO);
+                            if (noCheckeds.size() == (parentNode.getChildCount() - 2)) {
+                                ((StateObject)emptyCheckeds.get(0)).setCheck(CHECK_YES);
+                            }
+                        }
+                    }
+                    repaint();
+                }
+            }
+        }
 	}
 }
