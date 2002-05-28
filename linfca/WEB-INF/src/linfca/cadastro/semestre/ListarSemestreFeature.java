c81cec5a2db76a56886814476e29c2a734867f76
rommelnc <rommelnc@edda2279-5339-4143-a38b-a3a7c4ae352d>
2002-05-28 02:24:41 +0000

    *** empty log message ***

--- linfca/WEB-INF/src/linfca/cadastro/semestre/ListarSemestreFeature.java
+++ linfca/WEB-INF/src/linfca/cadastro/semestre/ListarSemestreFeature.java
@@ -4,10 +4,11 @@ package linfca.cadastro.semestre;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Date;
+import java.util.StringTokenizer;
 
 import linfca.Controller;
 import linfca.Feature;
-
 import org.jdom.Element;
 
 public class ListarSemestreFeature implements Feature {
@@ -18,33 +19,80 @@ public class ListarSemestreFeature implements Feature {
 	 * 
 	 * <out>
 	 * 	  <semestre>
-	 * 		<cod-semestre>1</cod-semestre>
-	 * 		<descricao-semestre>Aluno</descricao-semestre>
+	 * 		 <cod-semestre>1</cod-semestre>
+	 * 		 <descricao-semestre>01/2002</descricao-semestre>
+	 *       <dia-inicio>01</dia-inicio>
+	 *       <mes-inicio>03</mes-inicio>
+	 *       <ano-inicio>2002</ano-inicio>
+	 *       <dia-inicio>01</dia-inicio>
+	 *       <mes-inicio>07</mes-inicio>
+	 *       <ano-inicio>2002</ano-inicio>
 	 * 	  </semestre>*
 	 * </out> 
 	 * </pre>
 	 * @see Feature#process(Element)
 	 */
 	public Element process(Element in) throws Exception {
+		
 		Connection con = Controller.getInstance().makeConnection();
+		
 		PreparedStatement ps = con.prepareStatement("select * from semestre");
 		ResultSet rs = ps.executeQuery();
+		
 		Element out = new Element("out");		
 		while (rs.next()) {
-			Element semestre = new Element("semestre");
-			int codigo = rs.getInt("cod_semestre");
+			
+			int codigo = rs.getInt("cod_semestre");			
 			String descricao = rs.getString("desc_semestre");
-			Element codigoXML = new Element("cod-semestre");
-			codigoXML.setText("" + codigo);			
-			semestre.getChildren().add(codigoXML);
+			Date dataInicio = rs.getDate("data-inicio");
+			Date dataFim = rs.getDate("data-fim");
+			
+			StringTokenizer st = new StringTokenizer(dataInicio.toString(), "-");
+			String anoInicio = st.nextToken();
+			String mesInicio = st.nextToken();
+			String diaInicio = st.nextToken();
+			
+			st = new StringTokenizer(dataFim.toString(), "-");
+			String anoFim = st.nextToken();
+			String mesFim = st.nextToken();
+			String diaFim = st.nextToken();
+			
+			Element semestre = new Element("semestre");
 			Element descricaoXML = new Element("descricao-semestre");
+			Element codigoXML = new Element("cod-semestre");
+			Element diaInicioXML = new Element("dia-inicio");
+			Element mesInicioXML = new Element("mes-inicio");
+			Element anoInicioXML = new Element("ano-inicio");
+			Element diaFimXML = new Element("dia-fim");
+			Element mesFimXML = new Element("mes-fim");
+			Element anoFimXML = new Element("ano-fim");
+			
+			codigoXML.setText("" + codigo);
 			descricaoXML.setText(descricao);
+			diaInicioXML.setText(diaInicio);
+			mesInicioXML.setText(mesInicio);
+			anoInicioXML.setText(anoInicio);
+			diaFimXML.setText(diaFim);
+			mesFimXML.setText(mesFim);
+			anoFimXML.setText(anoFim);
+			
+			semestre.getChildren().add(codigoXML);
 			semestre.getChildren().add(descricaoXML);
+			semestre.getChildren().add(diaInicioXML);
+			semestre.getChildren().add(mesInicioXML);
+			semestre.getChildren().add(anoInicioXML);
+			semestre.getChildren().add(diaFimXML);
+			semestre.getChildren().add(mesFimXML);
+			semestre.getChildren().add(anoFimXML);
+			
 			out.getChildren().add(semestre);
+			
 		}
+		
 		rs.close();
 		ps.close();
 		con.close();
+		
 		return out;
 	}
 }
