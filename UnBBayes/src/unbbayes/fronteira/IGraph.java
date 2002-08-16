ba90714b69a2ca0b6761a91f3c4af95ff2412ca6
rommelnc <rommelnc@edda2279-5339-4143-a38b-a3a7c4ae352d>
2002-08-16 00:52:05 +0000

    *** empty log message ***

--- UnBBayes/src/unbbayes/fronteira/IGraph.java
+++ UnBBayes/src/unbbayes/fronteira/IGraph.java
@@ -152,10 +152,11 @@ public class IGraph extends JPanel implements MouseListener, MouseMotionListener
         arcColor = Color.black;
         selectionColor = Color.red;
         backColor = Color.white;
+        System.out.println(controller.getRede().getRadius());
 		if (controller.getRede().getRadius() > 40) {
             radius = 40;
 		} else if (controller.getRede().getRadius() < 10) {
-			radius = 10;
+			radius = 20;
 		} else {
 		    radius = controller.getRede().getRadius();
 		}
