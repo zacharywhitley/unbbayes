030ba70b57bd572400cc23536d4753874102dbd3
nobody <nobody@edda2279-5339-4143-a38b-a3a7c4ae352d>
2002-09-25 15:25:04 +0000

    This commit was manufactured by cvs2svn to create tag 'Version08'.

--- UnBBayes/src/Start.java
+++
@@ -1,7 +0,0 @@
-import unbbayes.datamining.gui.InvokerTest;
-
-public class Start
-{	public static void main(String[] args)
-	{	InvokerTest inv = new InvokerTest();
-	}
-}
--- linfca/WEB-INF/src/JavaBeep.java
+++
@@ -1,8 +0,0 @@
-import java.applet.Applet;
-import java.awt.Toolkit;
-
-public class JavaBeep extends Applet {
-	public void oneBeep() {
-	    Toolkit.getDefaultToolkit().beep();
-    }
-}
