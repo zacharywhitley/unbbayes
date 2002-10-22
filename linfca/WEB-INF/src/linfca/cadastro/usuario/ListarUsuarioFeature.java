9aae428f3ca1066edaac76bcfbcbe6cc82f87f90
rommelnc <rommelnc@edda2279-5339-4143-a38b-a3a7c4ae352d>
2002-10-22 16:51:17 +0000

    *** empty log message ***

---
+++ linfca/WEB-INF/src/linfca/cadastro/usuario/ListarUsuarioFeature.java
@@ -0,0 +1,80 @@
+
+package linfca.cadastro.usuario;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Iterator;
+
+import linfca.Controller;
+import linfca.Feature;
+
+import org.jdom.Element;
+
+public class ListarUsuarioFeature implements Feature {
+
+	/**
+	 * <pre>
+	 * <in/>
+	 * 
+	 * <out>
+	 * 	  <usuario>
+	 * 		 <cod-usuario>1</cod-usuario>
+	 *       <identificacao>9912345</identificacao>
+	 *       <nome-completo>Mané Peregrino</nome-completo>
+	 * 	  </usuario>*
+	 * </out> 
+	 * </pre>
+	 * @see Feature#process(Element)
+	 */
+	public Element process(Element in) throws Exception {
+		
+		Connection con = Controller.getInstance().makeConnection();
+		
+		PreparedStatement ps = con.prepareStatement("select * from usuario order by identificacao");
+		ResultSet rs = ps.executeQuery();
+		
+		Element out = new Element("out");			
+		
+		while (rs.next()) {
+			
+			Element usuario = new Element("usuario");
+			
+			int codigo = rs.getInt("cod_usuario");
+			String identificacao = rs.getString("identificacao");
+			String nome = rs.getString("nome");
+			String sobrenome = rs.getString("sobrenome");
+			
+			Element codigoXML = new Element("cod-usuario");
+			codigoXML.setText("" + codigo);			
+			usuario.getChildren().add(codigoXML);
+			
+			Element identificacaoXML = new Element("identificacao");
+			identificacaoXML.setText(identificacao);
+			usuario.getChildren().add(identificacaoXML);
+			
+			Element nomeXML = new Element("nome-completo");
+			nomeXML.setText(nome + " " + sobrenome);			
+			usuario.getChildren().add(nomeXML);
+			
+			out.getChildren().add(usuario);
+			
+		}
+		
+		Iterator usuarios = out.getChildren().iterator();
+		while (usuarios.hasNext()) {
+			Element usuario = (Element) usuarios.next();
+			System.out.println("CodUsuarioF: " + ((Element)usuario.getChild("cod-usuario")).getText());
+			System.out.println("IdentificacaoF: " + ((Element)usuario.getChild("identificacao")).getText());
+			System.out.println("NomeF: " + ((Element)usuario.getChild("nome-completo")).getText());
+			//System.out.println("SobreNomeF: " + ((Element)usuario.getChild("sobrenome")).getText());
+		}
+		
+		rs.close();
+		ps.close();
+		con.close();
+		
+		return out;
+	}
+
+}
