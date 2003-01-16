7b798eee0c9d5def38e9d87ca19a037342dc7819
danilocs <danilocs@edda2279-5339-4143-a38b-a3a7c4ae352d>
2003-01-16 00:47:23 +0000

    The field description is set

--- UnBBayes/src/unbbayes/aprendizagem/AlgorithmController.java
+++ UnBBayes/src/unbbayes/aprendizagem/AlgorithmController.java
@@ -2,8 +2,37 @@
 package unbbayes.aprendizagem;
 import unbbayes.util.NodeList;
 
+/*
+ *  UnbBayes
+ *  Copyright (C) 2002 Universidade de Brasília
+ *
+ *  This file is part of UnbBayes.
+ *
+ *  UnbBayes is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  UnbBayes is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with UnbBayes; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+/**
+ * @author Danilo 
+ * 
+ * This class is a factory that choose witch algorith the 
+ * program will use to learn the net.
+ */
 public class AlgorithmController{
 	
+	/**
+	 * 
+	 */
 	public AlgorithmController(NodeList variables,byte[][] matrix, int[] vector,
 	        long caseNumber, String[] pamp, boolean compacted){	        	
 	        if(pamp[0].equalsIgnoreCase("Ponctuation")){
