908b264f1511be22ac0bde250c3be416336589fd
mhpv <mhpv@edda2279-5339-4143-a38b-a3a7c4ae352d>
2003-02-09 11:41:11 +0000

    Sending all UnBBayes to CVS. I don´t know what happened

--- UnBBayes/src/unbbayes/datamining/classifiers/bayesianlearning/AlgorithmController.java
+++ UnBBayes/src/unbbayes/datamining/classifiers/bayesianlearning/AlgorithmController.java
@@ -3,6 +3,7 @@ package unbbayes.datamining.classifiers.bayesianlearning;
 import unbbayes.datamining.datamanipulation.*;
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 import unbbayes.prs.bn.*;
 import unbbayes.prs.*;
@@ -25,31 +26,45 @@ public class AlgorithmController {
 
     //testes
     ParametricLearning x = new ParametricLearning(instanceSet);
-    int[] pai = new int[2];
+    /*int[] pai = new int[2];
     pai[0]=1;
     pai[1]=2;
-    int [][] xibungo = x.computeNijk(0,pai);
+    int [][] xibungo = x.computeNijk(0,pai);*/
 
-    for (int i=0;i<xibungo.length;i++)
+    int[] array = new int[1];
+    array[0]=4;
+    ArrayList xumbrega = new ArrayList();
+    //xumbrega.add(new int[0]);   //pa[0]
+    xumbrega.add(array);   //pa[0]
+    xumbrega.add(array);   //pa[1]
+    xumbrega.add(array);   //pa[2]
+    xumbrega.add(array);        //pa[3]
+    xumbrega.add(new int[0]);        //pa[4]
+    //xumbrega = x.prob(xumbrega);
+    
+    /*for (int i=0;i<xumbrega.size();i++)
     {
-    	for (int j=0;j<xibungo[i].length;j++)
-    		System.out.println("teste "+xibungo[i][j]);
-    	System.out.println("fim teste ");
-    }
-    
-    
-    ProbabilisticNetwork net = new ProbabilisticNetwork("mario1213");
-    ProbabilisticNode node = new ProbabilisticNode();
-    node.setName("mario");
-    net.addNode(node);
+    	float[][] pjk = (float[][])xumbrega.get(i);
+    	System.out.println("variável i");
+    	for (int j=0;j<pjk.length;j++)
+    	{
+    		for (int k=0;k<pjk[j].length;k++)
+    		{
+    			System.out.println(pjk[j][k]);
+    		}
+    		System.out.println("mais");    		
+    	}    	
+    }*/
+
+    ProbabilisticNetwork net = x.getProbabilisticNetwork(xumbrega);
 
     netWindow = new NetWindow(net);
-    /*NetWindowEdition edition = netWindow.getNetWindowEdition();
-    edition.getCenterPanel().setBottomComponent(netWindow.getJspGraph());*/
-    EditNet editNet = netWindow.getEditNet();
-    editNet.getCenterPanel().setBottomComponent(netWindow.getJspGraph());
+    NetWindowEdition edition = netWindow.getNetWindowEdition();
+    edition.getCenterPanel().setBottomComponent(netWindow.getJspGraph());
+    /*EditNet editNet = netWindow.getEditNet();
+    editNet.getCenterPanel().setBottomComponent(netWindow.getJspGraph());*/
 
-    netWindow.changeToEditNet();
+    //netWindow.changeToEditNet();
   }
 
   public NetWindow getNetWindow()
--- UnBBayes/src/unbbayes/datamining/classifiers/bayesianlearning/ParametricLearning.java
+++ UnBBayes/src/unbbayes/datamining/classifiers/bayesianlearning/ParametricLearning.java
@@ -1,6 +1,8 @@
 package unbbayes.datamining.classifiers.bayesianlearning;
 
 import unbbayes.datamining.datamanipulation.*;
+import unbbayes.prs.bn.*;
+import unbbayes.prs.*;
 import java.util.*;
 
 public class ParametricLearning
@@ -69,17 +71,14 @@ public class ParametricLearning
 
 //----------------------------------------------------------------------------//
 
-  /* int[] instanciaPai(i,pai) */
-
-//----------------------------------------------------------------------------//
-
-  int [] multiplies (int [] pai)
+  public int [] multiplies (int [] pai)
   {
     int ip;                     //índice do pai da interação atual
     int np = pai.length;        //número de elementos do pai
-    int[] mult = new int[np];   //matriz de multiplicadores - tamanho m
+    int[] mult;   //matriz de multiplicadores - tamanho np
 
     np = (np==0)? 1: np;
+    mult = new int[np];
     mult[np-1] = 1;
     for(int k=np-2;k>=0;k--)
     {
@@ -111,11 +110,8 @@ public class ParametricLearning
     int ip;                        //índice do pai
     int fc;                        //frequência da linha atual
     int qi = computeQ(pai);        //quantidade de estados em 'pai'
-    System.out.println("qi = "+qi);
     int ri = dx[i].size();         //quantidade de estados da variável 'i'
     int[] mult = multiplies(pai);  //multiplicadores - para linearizar arranjo
-    for (int w=0;w<mult.length;w++)
-    	System.out.println("mult = "+mult[w]);
     int[][] njk = new int[qi][ri]; //frequência do estado k com pais no estado j
     int n = x.length;              //número de variáveis de 'x'
     int nd = d.length;             //observações distintas na amostra condensada
@@ -141,29 +137,117 @@ public class ParametricLearning
 
 //----------------------------------------------------------------------------//
 
-  /*
-  ProbNet probs()
-  {
-  }
-  */
+//parâmetro pa: ArrayList de arrays de inteiros
+//retorno: ArrayList de arrays bidimensionais de float
+public ArrayList prob(ArrayList pa)
+{
+  ArrayList nijk;	//ocorrência dos estados das variáveis relativos aos estados dos pais
+  int n = x.length;	//número de variáveis
+  int ri;		//número de estados da variável atual
+  int qi;		//número de estados dos pais da variável atual
+  int nij;		//número total de ocorrências da variável em determinado estado dos pais
+  ArrayList p;		//distribuição condicional das famílias	(ArrayList de arrays bidimensionais)
+  float[][] pjk;	//distribuição condicional da variável atual
+  int[][] njk;		//nijk para variável atual(i)
 
+  //calcular nijk
+  nijk = new ArrayList();
+  for (int i=0;i<n;i++)
+  {
+    nijk.add(computeNijk(i, (int[])pa.get(i)));
+  }
+
+  p = new ArrayList();
+  for(int i=0;i<n;i++)
+  {
+    ri = dx[i].size();
+    qi = computeQ((int[])pa.get(i));
+    pjk = new float[qi][ri];
+    njk = (int[][])nijk.get(i);
+
+    for(int j=0;j<qi;j++)
+    {
+      nij = 0;
+      for(int k=0;k<ri;k++)
+      {
+        nij = nij + njk[j][k];
+      }
+      for(int k=0;k<ri;k++)
+      {
+	pjk[j][k] = (float)(1+njk[j][k])/(float)(ri+nij);
+      }
+    }
+    p.add(pjk);
+  }
+  return p;
+}
+//----------------------------------------------------------------------------//
+public ProbabilisticNetwork getProbabilisticNetwork(ArrayList pa)
+{
+	ProbabilisticNetwork net = new ProbabilisticNetwork("net");
+    int width = 50;
+   	for(int i=0; i<x.length; i++)
+    {
+    	// Criação do nó
+    	ProbabilisticNode node = new ProbabilisticNode(); 
+      	node.setDescription(x[i]);
+      	node.setName(x[i]);
+      	node.setPosicao(width,100);
+      	width += 100;
+      	ArrayList states = dx[i];
+      	for (int j=0;j<states.size();j++)
+      	{   node.appendState(""+states.get(j));
+      	}
+      	// Criação do Tabela de probabilidades
+      	PotentialTable tab = node.getPotentialTable();  
+      	tab.addVariable(node);
+		net.addNode(node);
+    }
+   	for(int i=0; i<x.length; i++)
+    {
+    	ProbabilisticNode node = (ProbabilisticNode)net.getNodeAt(i); 
+      	PotentialTable tab = node.getPotentialTable();  
+      	int[] parents = (int[])pa.get(i);
+      	for (int j=0;j<parents.length;j++)
+      	{
+      		Edge arco = new Edge(net.getNodeAt(parents[j]),node);
+      		net.addEdge(arco);      		
+      	}      	
+      	
+      	ArrayList probs = prob(pa);
+      	float[][] pjk = (float[][])probs.get(i);
+    	// Inserção dos valores na tabela de probabilidades
+      	int counter = 0;
+        for (int j=0;j<pjk.length;j++)
+    	{
+    		for (int k=0;k<pjk[j].length;k++)
+    		{
+    			tab.setValue(counter,pjk[j][k]);
+    			counter++;
+    		}    		
+    	}    	
+
+    }
+    
+    return net;
+}
 //----------------------------------------------------------------------------//
 
   /** variáveis do problema */
-  String[] x;
+  private String[] x;
 
   /** domínios das variáveis de x
    * Array de ArrayLists de Strings
    * dx[i]: domínios da variável x[i] */
-  ArrayList[] dx;
+  private ArrayList[] dx;
 
   /** amostra condensada
    * d[ic,i]=v, sendo 'ic' a linha, 'i' o índice da variável e 'v' o valor da
    * variável na linha (representado pelo seu índice no domínio da variável)) */
-  short[][] d;
+  private short[][] d;
 
   /** frequencias das instâncias de d
    * freq[i]: frequência da linha(ou instancia) i */
-  int[] freq;
+  private int[] freq;
 
 }
