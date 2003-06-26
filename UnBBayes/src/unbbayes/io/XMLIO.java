f15099e0f7e06beb0e6197f56ffecaf20f514c9d
michael_onishi <michael_onishi@edda2279-5339-4143-a38b-a3a7c4ae352d>
2003-06-26 21:44:21 +0000

    *** empty log message ***

--- UnBBayes/src/unbbayes/io/XMLIO.java
+++ UnBBayes/src/unbbayes/io/XMLIO.java
@@ -11,11 +11,18 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+import javax.xml.transform.TransformerException;
+
+import org.apache.xpath.XPath;
+import org.apache.xpath.XPathAPI;
 import org.w3c.dom.Document;
+import org.w3c.dom.traversal.NodeIterator;
 import org.xml.sax.InputSource;
 
 import unbbayes.util.*;
 import unbbayes.prs.bn.Network;
+import unbbayes.prs.bn.ProbabilisticNode;
+import unbbayes.prs.Node;
 import unbbayes.prs.bn.ProbabilisticNetwork;
 import unbbayes.prs.msbn.SingleAgentMSBN;
 
@@ -26,19 +33,43 @@ import unbbayes.prs.msbn.SingleAgentMSBN;
  * Window>Preferences>Java>Code Generation>Code and Comments
  */
 public class XMLIO implements BaseIO {
+	
+	private static String NET_NAME = "/BIF/HEADER/NAME",
+							NODE_SIZE = "/BIF/STATICPROPERTY/NODESIZE",
+							VARIABLES = "/BIF/NETWORK/VARIABLES/*";
+							
+	private static Node makeNode(org.w3c.dom.Node elNode) throws Exception {
+		Node node = null;
+		
+		node = new ProbabilisticNode();
+		
+		org.w3c.dom.Node tmpNode = XPathAPI.selectSingleNode(elNode, "/VAR/LABEL");
+		node.setName(XMLUtil.getValue(tmpNode));
+		
+		NodeIterator states = XPathAPI.selectNodeIterator(elNode, "VAR/STATENAME");
+		while ((elNode = states.nextNode()) != null) {
+			node.appendState(XMLUtil.getValue(elNode));
+		}
+		
+		return node;				
+	}
 
 	/* (non-Javadoc)
 	 * @see unbbayes.io.BaseIO#load(java.io.File)
 	 */
 	public ProbabilisticNetwork load(File input) throws LoadException, IOException {
 		ProbabilisticNetwork net = null;
-		try {
-			
+		org.w3c.dom.Node elNode = null;
+		try {			
 			InputSource is = new InputSource(new FileReader(input));
-			Document doc = XMLUtil.getDocument(is);
-			String xPathName = "//BIF/HEADER/NAME/value";			
-			net = new ProbabilisticNetwork("");
-						
+			Document doc = XMLUtil.getDocument(is);			
+			elNode = XPathAPI.selectSingleNode(doc, NET_NAME);
+			net = new ProbabilisticNetwork(XMLUtil.getValue(elNode));
+			
+			NodeIterator nodeIterator = XPathAPI.selectNodeIterator(doc, VARIABLES);
+			while ((elNode = nodeIterator.nextNode()) != null) {
+				net.addNode(makeNode(elNode));
+			}
 			
 		} catch (Exception e) {
 			throw new LoadException("Load Error");			
