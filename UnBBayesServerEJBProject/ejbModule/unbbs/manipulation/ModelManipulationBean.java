990dd775e14303b890dbbbe9ace6f21e9b9249e2
michael_onishi <michael_onishi@edda2279-5339-4143-a38b-a3a7c4ae352d>
2003-07-12 17:50:07 +0000

    complete - not tested

--- UnBBayesServerEJBProject/ejbModule/unbbs/manipulation/ModelManipulationBean.java
+++ UnBBayesServerEJBProject/ejbModule/unbbs/manipulation/ModelManipulationBean.java
@@ -1,12 +1,13 @@
 package unbbs.manipulation;
 
-import java.io.File;
-import java.io.IOException;
-
 import javax.ejb.FinderException;
 import javax.naming.NamingException;
+import javax.xml.transform.TransformerException;
 
-import unbbayes.io.BaseIO;
+import org.apache.xpath.CachedXPathAPI;
+import org.w3c.dom.Document;
+import org.w3c.dom.traversal.NodeIterator;
+
 import unbbayes.io.XMLIO;
 import unbbayes.prs.bn.ProbabilisticNetwork;
 import unbbayes.prs.bn.TreeVariable;
@@ -20,36 +21,11 @@ import unbbs.persistence.model.ModelLocalHome;
  * Bean implementation class for Enterprise Bean: ModelManipulation
  */
 public class ModelManipulationBean implements javax.ejb.SessionBean {
-
 	public String compile(int modelId) {
 		try {
-			ModelLocalHome modelLocalHome =
-				(ModelLocalHome) HomeFactory.singleton().getHome("ejb/Model");
-
-			ModelLocal modelLocal =
-				modelLocalHome.findByPrimaryKey(new ModelKey(modelId));
-			String model = modelLocal.getModel();
-			XMLIO io = new XMLIO();
-			ProbabilisticNetwork net = io.load(XMLUtil.getDocument(model));
+			ProbabilisticNetwork net = getNetwork(modelId);
 			net.compile();
-
-			StringBuffer xml = new StringBuffer("<NET NAME='" + modelLocal.getName() + "' >");
-			for (int i = 0; i < net.getNodeCount(); i++) {
-				TreeVariable var = (TreeVariable) net.getNodeAt(i);
-				xml.append(
-					"<VARIABLE NAME='"
-						+ var.getName()
-						+ "' DESCRIPTION='"
-						+ var.getDescription()
-						+ "' >");
-				for (int j = 0; j < var.getStatesSize(); j++) {
-					xml.append("<STATE NAME='" + var.getStateAt(j) + "' MARGINAL='" + var.getMarginalAt(j) + "' />");
-				}
-				xml.append("</VARIABLE>");
-			}
-			xml.append("</NET>");
-			return xml.toString();
-
+			return makeMarginals(net);
 		} catch (NamingException e) {
 			e.printStackTrace();
 		} catch (FinderException e) {
@@ -58,6 +34,77 @@ public class ModelManipulationBean implements javax.ejb.SessionBean {
 			e.printStackTrace();
 		}
 		return null;
+	}
+
+	public String reinitialize(int modelId) {
+		return compile(modelId);
+	}
+
+	public String propagate(int modelId, Document evidences) {
+		try {
+			ProbabilisticNetwork net = getNetwork(modelId);
+			net.compile();
+			CachedXPathAPI xpath = new CachedXPathAPI();
+			NodeIterator nodeIterator =
+				xpath.selectNodeIterator(evidences, "/EVIDENCES/EVIDENCE");
+			org.w3c.dom.Node elNode;
+			while ((elNode = nodeIterator.nextNode()) != null) {
+				String nodeName = xpath.selectSingleNode(elNode, "@NODE").getNodeValue();
+				String state = xpath.selectSingleNode(elNode, "@STATE").getNodeValue();
+				TreeVariable var = (TreeVariable) net.getNode(nodeName);
+				var.addFinding(Integer.parseInt(state));
+			}
+			net.updateEvidences();
+			return makeMarginals(net);
+			
+		} catch (TransformerException e) {
+			e.printStackTrace();
+		} catch (NamingException e) {
+			e.printStackTrace();
+		} catch (FinderException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private String makeMarginals(ProbabilisticNetwork net) {
+		StringBuffer xml =
+			new StringBuffer("<NET NAME='" + net.getName() + "' >");
+		for (int i = 0; i < net.getNodeCount(); i++) {
+			TreeVariable var = (TreeVariable) net.getNodeAt(i);
+			xml.append(
+				"<VARIABLE NAME='"
+					+ var.getName()
+					+ "' DESCRIPTION='"
+					+ var.getDescription()
+					+ "' >");
+			for (int j = 0; j < var.getStatesSize(); j++) {
+				xml.append(
+					"<STATE NAME='"
+						+ var.getStateAt(j)
+						+ "' MARGINAL='"
+						+ var.getMarginalAt(j)
+						+ "' />");
+			}
+			xml.append("</VARIABLE>");
+		}
+		xml.append("</NET>");
+		return xml.toString();
+	}
+
+	private ProbabilisticNetwork getNetwork(int modelId)
+		throws NamingException, FinderException, Exception {
+
+		ModelLocalHome modelLocalHome =
+			(ModelLocalHome) HomeFactory.singleton().getHome("ejb/Model");
+
+		ModelLocal modelLocal =
+			modelLocalHome.findByPrimaryKey(new ModelKey(modelId));
+		String model = modelLocal.getModel();
+		XMLIO io = new XMLIO();
+		return io.load(XMLUtil.getDocument(model));
 	}
 
 	private javax.ejb.SessionContext mySessionCtx;
