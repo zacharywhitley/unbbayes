8a808bcec7fda2e437bfaa81764a14ee92688be9
rommelnc <rommelnc@edda2279-5339-4143-a38b-a3a7c4ae352d>
2006-12-11 13:17:36 +0000

    Minor changes.

--- UnBBayes/src/unbbayes/prs/mebn/entity/CategoricalStatesEntity.java
+++ UnBBayes/src/unbbayes/prs/mebn/entity/CategoricalStatesEntity.java
@@ -20,11 +20,12 @@ public class CategoricalStatesEntity extends Entity {
 	private static List<CategoricalStatesEntity> listEntity;
 	
 	public static void addEntity(CategoricalStatesEntity entity) {
-		listEntity.add(entity);
+		CategoricalStatesEntity.listEntity.add(entity);
 	}
 	
+	// TODO Possivelmente fazer alguma limpeza de variáveis ou dependências aqui.
 	public static void removeEntity(CategoricalStatesEntity entity) {
-		listEntity.remove(entity);
+		CategoricalStatesEntity.listEntity.remove(entity);
 	}
 
 }
