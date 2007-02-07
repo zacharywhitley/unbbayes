45273a6abff3d3decec63ccb60b9070b72772789
rommelnc <rommelnc@edda2279-5339-4143-a38b-a3a7c4ae352d>
2007-02-07 02:23:35 +0000

    Exception changed to be able to throw nested exceptions.

--- UnBBayes/src/unbbayes/prs/mebn/exception/MEBNException.java
+++ UnBBayes/src/unbbayes/prs/mebn/exception/MEBNException.java
@@ -2,8 +2,17 @@ package unbbayes.prs.mebn.exception;
 
 public class MEBNException extends Exception{
     
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6287201364470449002L;
+
 	public MEBNException(String e){
     	super(e); 
     }
+
+	public MEBNException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
 	
 }
