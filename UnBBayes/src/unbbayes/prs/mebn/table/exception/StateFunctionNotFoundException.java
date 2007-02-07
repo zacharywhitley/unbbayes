7449334cb58faf50185ad432e19e8abb68a2e53a
rommelnc <rommelnc@edda2279-5339-4143-a38b-a3a7c4ae352d>
2007-02-07 02:26:41 +0000

    When the function of the given state is not found.

---
+++ UnBBayes/src/unbbayes/prs/mebn/table/exception/StateFunctionNotFoundException.java
@@ -0,0 +1,16 @@
+package unbbayes.prs.mebn.table.exception;
+
+import unbbayes.prs.mebn.exception.MEBNException;
+
+public class StateFunctionNotFoundException extends MEBNException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2953649904897403966L;
+
+	public StateFunctionNotFoundException(String msg) {
+		super(msg);
+	}
+	
+}
