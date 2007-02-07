6f3b2129472f74a4f65281dbaf724775ed43178e
rommelnc <rommelnc@edda2279-5339-4143-a38b-a3a7c4ae352d>
2007-02-07 02:28:12 +0000

    Adjusting to fit the new way to eval the state function by its ProbabilityFunctionOperator.

--- UnBBayes/src/unbbayes/prs/mebn/table/ProbabilityFunction.java
+++ UnBBayes/src/unbbayes/prs/mebn/table/ProbabilityFunction.java
@@ -1,15 +1,20 @@
 package unbbayes.prs.mebn.table;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import unbbayes.prs.mebn.MultiEntityNode;
+import unbbayes.prs.mebn.table.exception.ProbabilityFunctionEvalException;
+import unbbayes.prs.mebn.table.exception.StateFunctionNotFoundException;
 
 public class ProbabilityFunction {
 	
 	private MultiEntityNode node;
-	private StateFunction[] listStateFunction;
+	private Map<String, StateFunction> mapStateFunction;
 
 	public ProbabilityFunction(MultiEntityNode node) {
 		this.node = node;
-		listStateFunction = new StateFunction[node.getStatesSize()];
+		mapStateFunction = new HashMap<String, StateFunction>(node.getStatesSize());
 	}
 	
 	/**
@@ -18,9 +23,23 @@ public class ProbabilityFunction {
 	 * @param stateFunction The state function to be added.
 	 */
 	public void addStateFunction(StateFunction stateFunction) {
-		// Set the state function to the index it is found in the node.
-		int index = node.getPossibleValueIndex(stateFunction.getStateName());
-		listStateFunction[index] =  stateFunction;
+		mapStateFunction.put(stateFunction.getStateName(), stateFunction);
+	}
+	
+	/**
+	 * Method responsible for returning the evaluation of the given state's 
+	 * function.
+	 * @param stateName The name of the state's function
+	 * @return The evaluation of the given state's function.
+	 * @throws ProbabilityFunctionEvalException
+	 * @throws StateFunctionNotFoundException 
+	 */
+	public float evalStateFunction(String stateName) throws ProbabilityFunctionEvalException, StateFunctionNotFoundException {
+		StateFunction function = mapStateFunction.get(stateName);
+		if (function == null) {
+			throw new StateFunctionNotFoundException("The state's function " + stateName + " was not found!");
+		}
+		return function.eval();
 	}
 
 }
--- UnBBayes/src/unbbayes/prs/mebn/table/StateFunction.java
+++ UnBBayes/src/unbbayes/prs/mebn/table/StateFunction.java
@@ -4,16 +4,20 @@ import java.util.List;
 
 import unbbayes.prs.mebn.MultiEntityNode;
 import unbbayes.prs.mebn.exception.EntityNotPossibleValueOfNodeException;
+import unbbayes.prs.mebn.table.exception.InvalidProbabilityFunctionOperandException;
+import unbbayes.prs.mebn.table.exception.ProbabilityFunctionEvalException;
 
 public class StateFunction {
 	
+	private IfClause ifClause;
 	private MultiEntityNode node;
 	private String stateName;
-	private List<Object> function;
+	private ProbabilityFunctionOperator function;
 
-	public StateFunction(MultiEntityNode node, String stateName) throws EntityNotPossibleValueOfNodeException {
+	public StateFunction(MultiEntityNode node, String stateName, IfClause ifClause) throws EntityNotPossibleValueOfNodeException {
 		this.node = node;
 		this.stateName = stateName;
+		this.ifClause = ifClause;
 		if (!node.hasPossibleValue(stateName)) {
 			throw new EntityNotPossibleValueOfNodeException("The entity " + stateName + " is not present in the node " + node.getName());
 		}
@@ -24,11 +28,55 @@ public class StateFunction {
 	}
 	
 	/**
-	 * Method responsible for adding elements in the function.
-	 * @param element The element to be added.
+	 * Method responsible for setting the function.
+	 * @param function The function to be set.
+	 * @throws InvalidProbabilityFunctionOperandException 
 	 */
-	public void addFunctionElement(Object element) {
-		function.add(element);
+	public void setFunction(ProbabilityFunctionOperator function) throws InvalidProbabilityFunctionOperandException {
+		if (this.function != null) {
+			ProbabilityFunctionOperator tempFunction = function;
+			while(tempFunction.getSecondOperand() != null) {
+				tempFunction = tempFunction.getSecondOperand(); 
+			}
+			tempFunction.setSecondOperand(function);
+		} else {
+			this.function = function;
+		}
+	}
+	
+	/**
+	 * Method responsible for setting a default function when the function is 
+	 * actually just a number. So it is created a 
+	 * ProbabilityFunctionOperator.PLUS with the second operand being 0.
+	 * @param number The number to be added to zero. The function itself.
+	 * @throws InvalidProbabilityFunctionOperandException 
+	 */
+	public void setFunction(float number) throws InvalidProbabilityFunctionOperandException {
+		if (this.function != null) {
+			ProbabilityFunctionOperator tempFunction = function;
+			while(tempFunction.getSecondOperand() != null) {
+				tempFunction = tempFunction.getSecondOperand(); 
+			}
+			tempFunction.setSecondOperand(number);
+		} else {
+			this.function = ProbabilityFunctionOperator.PLUS;
+			function.setFirstOperand(number);
+			function.setSecondOperand(0);
+		}
+	}
+	
+	/**
+	 * Method responsible for evaluating this states function and returning a 
+	 * number.
+	 * @return The resulting number of the function's evaluation.
+	 * @throws ProbabilityFunctionEvalException
+	 */
+	public float eval() throws ProbabilityFunctionEvalException {
+		return function.eval();
+	}
+
+	public IfClause getIfClause() {
+		return ifClause;
 	}
 
 }
