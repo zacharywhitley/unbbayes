2964034c1d3916a63158af99cfc1d349e05c6681
laecio_lima <laecio_lima@edda2279-5339-4143-a38b-a3a7c4ae352d>
2007-02-21 00:10:05 +0000

    GUI Mebn - Refatoração e Melhoramentos

---
+++ UnBBayes/src/unbbayes/prs/mebn/entity/exception/CategoricalStateDoesNotExistException.java
@@ -0,0 +1,19 @@
+package unbbayes.prs.mebn.entity.exception;
+
+public class CategoricalStateDoesNotExistException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5036670577194690304L;
+
+	public CategoricalStateDoesNotExistException() {
+		super();
+	}
+	
+	public CategoricalStateDoesNotExistException(String msg) {
+		super(msg);
+	}
+	
+	
+}
