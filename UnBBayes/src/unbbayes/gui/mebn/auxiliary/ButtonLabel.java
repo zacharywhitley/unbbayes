732e477562528b0ce6f5b7d293604f05abd6619a
laecio_lima <laecio_lima@edda2279-5339-4143-a38b-a3a7c4ae352d>
2007-05-30 03:20:02 +0000

    Botão para representar qual tipo de nó esta sendo atualmente editado

---
+++ UnBBayes/src/unbbayes/gui/mebn/auxiliary/ButtonLabel.java
@@ -0,0 +1,28 @@
+package unbbayes.gui.mebn.auxiliary;
+
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+import unbbayes.controller.IconController;
+
+public class ButtonLabel extends JButton{
+	
+	private int SIZE_LABEL = 25; 
+	
+	public ButtonLabel(String label, ImageIcon icon){
+		super(); 
+		int sizeLabel = label.length();
+		if(sizeLabel < SIZE_LABEL){
+			for(int i = sizeLabel; i < SIZE_LABEL; i++){
+				label+=" "; 
+			}
+		}
+		setIcon(icon); 
+		setText(label); 
+    	setBackground(new Color(193, 210, 205)); //Verde quase cinza
+    	setForeground(Color.BLACK);
+    }
+	
+}
