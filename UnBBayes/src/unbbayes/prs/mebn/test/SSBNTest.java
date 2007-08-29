26d27f24de1dd4ee9f6eff6a039b3d2e16804eb6
cardialfly <cardialfly@edda2279-5339-4143-a38b-a3a7c4ae352d>
2007-08-29 23:37:18 +0000

    *** empty log message ***

--- UnBBayes/src/unbbayes/prs/mebn/test/SSBNTest.java
+++ UnBBayes/src/unbbayes/prs/mebn/test/SSBNTest.java
@@ -2,9 +2,12 @@ package unbbayes.prs.mebn.test;
 
 import java.io.File;
 
+import unbbayes.controller.MEBNController;
 import unbbayes.io.mebn.PrOwlIO;
 import unbbayes.io.mebn.UbfIO;
 import unbbayes.prs.mebn.MultiEntityBayesianNetwork;
+import unbbayes.prs.mebn.kb.powerloom.PowerLoomKB;
+import unbbayes.util.Debug;
 
 public class SSBNTest {
 	
@@ -12,7 +15,7 @@ public class SSBNTest {
 		// 0. Comentar o ConsistencyUtilities.hasCycle()
 		// 1. Caregar a ontologia StarTrek27.ubf
 		PrOwlIO io = new PrOwlIO(); 
-		MultiEntityBayesianNetwork net = io.loadMebn(new File("examples/mebn/StarTrek28.owl"));
+		MultiEntityBayesianNetwork net = io.loadMebn(new File("examples/mebn/StarTrek30.owl"));
 		
 		System.out.println(net.getName());
 		
@@ -35,11 +38,51 @@ public class SSBNTest {
 		
 		
 		// 3. Popular a base de conhecimento com as evidencias
+		long tIni = System.currentTimeMillis();
+		PowerLoomKB kb = PowerLoomKB.getInstanceKB(); 
+		kb.loadDefinitionsFile("examples/mebn/starshipfull.plm");
+		long tFim = System.currentTimeMillis();
 		
-		
+		System.out.println("Carregou a kb em: " + (tFim - tIni)/1000f + " segundos" );
 		
 		// 4. Iniciar algoritmo de SSBN - QUERY
-		// 5. Procurar por !ST4 e !T0
+		
+		// 4.1 Pesquisar como fazer para retornar false para ask starship t0
+		
+		// STEP 1 - fazer esse passo depois do 3, pq nao sei os tipos de st4 e t0
+		tIni = System.currentTimeMillis();
+		String teste = kb.executeCommand("(ask (Starship_label !ST4))"); 
+		
+		if (!teste.equalsIgnoreCase("TRUE")) {
+			System.out.println("Não achou !st4");
+		}
+		tFim = System.currentTimeMillis();
+		
+		System.out.println("(ask (Starship_label !ST4)) em: " + (tFim - tIni)/1000f + " segundos" );
+		
+		teste = kb.executeCommand("(ask (Starship_label !ST4))"); 
+		
+		if (!teste.equalsIgnoreCase("TRUE")) {
+			System.out.println("Não achou !st4");
+		}
+		
+		// STEP 2
+		teste = kb.executeCommand("(RETRIEVE (HarmPotential !ST4 !T0 ?x) )"); 
+		if (!teste.equalsIgnoreCase("No solution.")) {
+			System.out.println("Solução econtrada é: " + teste);
+			return;
+		}
+		
+		// STEP 3
+		// criar um método q retorne a MFrag de HarmPotential (domain) 
+		// e verificar se os parametros passados batem com o tipo
+		
+		// STEP 4
+		// criar um metodo para recuperar apenas os nos de contexto q se referem a t e st
+		// avaliar cada no de conexto assumindo st4 e t0
+		
+		
+		
 	}
 
 }
