06f2797796ae6833654c87b473330cf9a928457a
blourenco <blourenco@edda2279-5339-4143-a38b-a3a7c4ae352d>
2007-09-20 23:00:03 +0000

    methodTest2()

--- CursoNivelamento/src/br/unb/cic/Nivelamento.java
+++ CursoNivelamento/src/br/unb/cic/Nivelamento.java
@@ -1,8 +1,24 @@
 package br.unb.cic;
 
 public class Nivelamento {
+
+	//constructor
+	Nivelamento(String teacher_name)
+	{
+		teacher=teacher_name;
+		grade=0;
+	}
+	public void methodTest2()
+	{
+		System.out.println("The grade is"+grade+"\n."+"The teacher is"+teacher+"\n.");
+	}
+	
+	
+	private double grade;
+	private String teacher;
 	
 	public void methodTest() {
 		System.out.println("teste");
 	}
+
 }
