1e2a41df8bd3836912d060e689c727ad4bb9ab4b
blourenco <blourenco@edda2279-5339-4143-a38b-a3a7c4ae352d>
2007-09-20 23:14:06 +0000

    Now we have a class to test Nivelamento.

---
+++ CursoNivelamento/src/br/unb/cic/NivelamentoTest.java
@@ -0,0 +1,11 @@
+package br.unb.cic;
+
+public class NivelamentoTest {
+
+
+	public static void main(String[] args) {
+		Nivelamento nivelamento=new Nivelamento("Teacher");
+		nivelamento.methodTest2();
+	}
+
+}
