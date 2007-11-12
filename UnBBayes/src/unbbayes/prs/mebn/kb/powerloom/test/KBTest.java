26c900fd70d3333f65417cc6518049b3e7e23fa7
laecio_lima <laecio_lima@edda2279-5339-4143-a38b-a3a7c4ae352d>
2007-11-12 01:13:08 +0000

    ...metodos comentados por desatualizacao.

--- UnBBayes/src/unbbayes/prs/mebn/kb/powerloom/test/KBTest.java
+++ UnBBayes/src/unbbayes/prs/mebn/kb/powerloom/test/KBTest.java
@@ -18,79 +18,79 @@ public class KBTest {
 	
 	public static void main(String[] args) throws Exception {
 		
-		PowerLoomKB kb = PowerLoomKB.getInstanceKB(); 
-		
-		kb.loadModule(new File("examples/mebn/sample.plm"));
-		KBFacade kbFacade = new PowerLoomFacade("/PL-KERNEL-KB/PL-USER/GENERATIVE_MODULE/FINDINGS_MODULE"); 
-		
-		
-		
-		
-		//getEntityByType
-		System.out.println("Retornando as Zone do KB:"); 
-		List<String> list = kbFacade.getEntityByType("ZONE_LABEL");
-		for(String entity: list){
-			System.out.println(" -> " + entity); 
-		}
-		
-		System.out.println(); 
-		
-		System.out.print("Retornando as Starships do KB:"); 
-		list = kbFacade.getEntityByType("STARSHIP_LABEL");
-		for(String entity: list){
-			System.out.println(" -> " + entity); 
-		}
-		
-		
-		
-		
-		
-//		searchFindings
-		System.out.println("Search findings for STARSHIPZONE !ST1:"); 
-		List<String> arguments = new ArrayList<String>(); 
-		arguments.add("!ST1"); 
-		String result = kbFacade.searchFinding("STARSHIPZONE", arguments);
-		if(result == null){
-			System.out.println(" -> Não encontrado"); 	
-		}else{
-			System.out.println(" -> " + result); 
-		}
-		
-		System.out.println(); 
-		
-		System.out.println("Search findings for STARSHIPZONE !ST4:"); 
-		arguments = new ArrayList<String>(); 
-		arguments.add("!ST4"); 
-		result = kbFacade.searchFinding("STARSHIPZONE", arguments);
-		if(result == null){
-			System.out.println(" -> Não encontrado"); 	
-		}else{
-			System.out.println(" -> Encontrado"); 
-		}
-		
-		
-		
-		
-		
-//		search Entities
-		System.out.println("Search entity !ST1:"); 
-		boolean resultB = kbFacade.existEntity("!ST1");
-		if(!resultB){
-			System.out.println(" -> Não encontrado"); 	
-		}else{
-			System.out.println(" -> Encontrado"); 
-		}
-		
-		System.out.println(); 
-		
-		System.out.println("Search entity !ST4:"); 
-		resultB = kbFacade.existEntity("ST4");
-		if(!resultB){
-			System.out.println(" -> Não encontrado"); 	
-		}else{
-			System.out.println(" -> " + result); 
-		}
-		
+//		PowerLoomKB kb = PowerLoomKB.getInstanceKB(); 
+//		
+//		kb.loadModule(new File("examples/mebn/sample.plm"));
+//		KBFacade kbFacade = new PowerLoomFacade("/PL-KERNEL-KB/PL-USER/GENERATIVE_MODULE/FINDINGS_MODULE"); 
+//		
+//		
+//		
+//		
+//		//getEntityByType
+//		System.out.println("Retornando as Zone do KB:"); 
+//		List<String> list = kbFacade.getEntityByType("ZONE_LABEL");
+//		for(String entity: list){
+//			System.out.println(" -> " + entity); 
+//		}
+//		
+//		System.out.println(); 
+//		
+//		System.out.print("Retornando as Starships do KB:"); 
+//		list = kbFacade.getEntityByType("STARSHIP_LABEL");
+//		for(String entity: list){
+//			System.out.println(" -> " + entity); 
+//		}
+//		
+//		
+//		
+//		
+//		
+////		searchFindings
+//		System.out.println("Search findings for STARSHIPZONE !ST1:"); 
+//		List<String> arguments = new ArrayList<String>(); 
+//		arguments.add("!ST1"); 
+//		String result = kbFacade.searchFinding("STARSHIPZONE", arguments);
+//		if(result == null){
+//			System.out.println(" -> Não encontrado"); 	
+//		}else{
+//			System.out.println(" -> " + result); 
+//		}
+//		
+//		System.out.println(); 
+//		
+//		System.out.println("Search findings for STARSHIPZONE !ST4:"); 
+//		arguments = new ArrayList<String>(); 
+//		arguments.add("!ST4"); 
+//		result = kbFacade.searchFinding("STARSHIPZONE", arguments);
+//		if(result == null){
+//			System.out.println(" -> Não encontrado"); 	
+//		}else{
+//			System.out.println(" -> Encontrado"); 
+//		}
+//		
+//		
+//		
+//		
+//		
+////		search Entities
+//		System.out.println("Search entity !ST1:"); 
+//		boolean resultB = kbFacade.existEntity("!ST1");
+//		if(!resultB){
+//			System.out.println(" -> Não encontrado"); 	
+//		}else{
+//			System.out.println(" -> Encontrado"); 
+//		}
+//		
+//		System.out.println(); 
+//		
+//		System.out.println("Search entity !ST4:"); 
+//		resultB = kbFacade.existEntity("ST4");
+//		if(!resultB){
+//			System.out.println(" -> Não encontrado"); 	
+//		}else{
+//			System.out.println(" -> " + result); 
+//		}
+//		
 		
 	}
 	
