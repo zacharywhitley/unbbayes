f25a719a380c692808f488247b2e743d40e3e158
rommelnc <rommelnc@edda2279-5339-4143-a38b-a3a7c4ae352d>
2008-11-13 16:59:25 +0000

    (feature:2272627) Some changes to make sure when the focus is gained by clicking the cell the value is not replaced by empty string, instead, just selects the whole text.

--- UnBBayes/src/main/java/unbbayes/gui/table/ReplaceTextCellEditor.java
+++ UnBBayes/src/main/java/unbbayes/gui/table/ReplaceTextCellEditor.java
@@ -1,22 +1,44 @@
 package unbbayes.gui.table;
 
 import java.awt.Component;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 import javax.swing.DefaultCellEditor;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 
 /**
- * Displays JLabel for "editing" - i.e. clicking.
+ * Class responsible for replacing the old value by the new value typed instead of appending it.
+ * However, if the focus is gained by something different than by typing a new value, then it 
+ * just selects all the text inside the text field component.
  */
 public class ReplaceTextCellEditor extends DefaultCellEditor {
 	private static final long serialVersionUID = 1L;
 
 	protected JTextField textField = new JTextField();
+	protected String previousValue = "";
 
 	public ReplaceTextCellEditor() {
 		super(new JTextField());
 		this.textField = (JTextField) this.editorComponent;
+		
+		this.textField.addFocusListener(new FocusAdapter() {
+			public void focusGained(FocusEvent e) {
+				// New value that the text field has.
+				String text = textField.getText();
+				// If it is the same, just select it all.
+				if (previousValue.equals(text)) {
+					textField.selectAll();
+				// If it is different than what it had before, than just replace the 
+				// previous value by the new value typed (text - previousValue). 
+				// Because text = previousValue + newValueTyped.
+				} else if (text.length() > 0 && previousValue.length() < text.length()) {
+					textField.setText(text.substring(previousValue.length()));
+				}
+			}
+		});
+		
 	}
 
 	public void setValue(Object value) {
@@ -27,7 +49,6 @@ public class ReplaceTextCellEditor extends DefaultCellEditor {
 		return textField.getText();
 	}
 
-	// Implement the one method defined by TableCellEditor.
 	public Component getTableCellEditorComponent(JTable table, Object value,
 			boolean isSelected, int row, int column) {
 		
@@ -35,7 +56,12 @@ public class ReplaceTextCellEditor extends DefaultCellEditor {
 			return null;
 		}
 
-		this.textField.setText("");
+		// Used to control what to do when the text filed gains focus (select all or replace text).
+		this.previousValue = value.toString();
+
+		this.textField.setText(this.previousValue);
+		
+		
 
 		return this.textField;
 	}
