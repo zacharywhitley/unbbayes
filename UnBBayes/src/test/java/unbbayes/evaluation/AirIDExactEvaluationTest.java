8e1ead797cafa950757092d240c84367d477c35a
rommelnc <rommelnc@edda2279-5339-4143-a38b-a3a7c4ae352d>
2009-03-28 00:53:56 +0000

    (feature:2717798) Test exact computation using example from Fusion 2009 paper, AirID. It takes about 75 minutes to run this test.

---
+++ UnBBayes/src/test/java/unbbayes/evaluation/AirIDExactEvaluationTest.java
@@ -0,0 +1,121 @@
+package unbbayes.evaluation;
+
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import unbbayes.evaluation.exception.EvaluationException;
+
+public class AirIDExactEvaluationTest {
+	
+	private static Evaluation evaluationExact;
+	
+	private final float DELTA = .0001f;
+	
+	@BeforeClass
+	public static void oneTimeSetUp() throws Exception {
+		boolean computeTime = true;
+		long time = 0;
+		
+		List<String> targetNodeNameList = new ArrayList<String>();
+		targetNodeNameList.add("TargetType");
+
+		List<String> evidenceNodeNameList = new ArrayList<String>();
+		evidenceNodeNameList.add("UHRR_Confusion");
+		evidenceNodeNameList.add("ModulationFrequency");
+		evidenceNodeNameList.add("CenterFrequency");
+		evidenceNodeNameList.add("PRI");
+		evidenceNodeNameList.add("PRF");
+
+		String netFileName = "src/test/resources/testCases/evaluation/AirID.xml";
+
+		evaluationExact = new Evaluation();
+		if (computeTime) {
+			time = System.currentTimeMillis();
+		}
+		evaluationExact.evaluate(netFileName, targetNodeNameList,
+				evidenceNodeNameList);
+		if (computeTime) {
+			time = System.currentTimeMillis() - time;
+			System.out.println("Time to compute evaluation: " + (float)(time)/1000 + " seconds");
+		}
+		
+	}
+	
+	@AfterClass
+	public static void oneTimeTearDown() throws Exception {
+		evaluationExact = null;
+	}
+	
+	@Test
+	public void testCM() {
+		float[][] cm = evaluationExact.getEvidenceSetCM();
+		
+		// CM
+		// [0.7655 0.1087 0.0353 0.0353 0.0517 0.0586] 
+		// [0.1087 0.7680 0.0348 0.0348 0.0503 0.0571] 
+		// [0.0353 0.0348 0.7661 0.1087 0.0517 0.0586] 
+		// [0.0353 0.0348 0.1087 0.7661 0.0517 0.0586] 
+		// [0.0258 0.0252 0.0258 0.0258 0.6094 0.1851] 
+		// [0.0293 0.0286 0.0293 0.0293 0.1851 0.5818] 
+		
+		// Columns add to one
+		assertEquals(.7655, cm[0][0], DELTA);
+		assertEquals(.1087, cm[1][0], DELTA);
+		assertEquals(.0353, cm[2][0], DELTA);
+		assertEquals(.0353, cm[3][0], DELTA);
+		assertEquals(.0258, cm[4][0], DELTA);
+		assertEquals(.0293, cm[5][0], DELTA);
+		
+		assertEquals(.1087, cm[0][1], DELTA);
+		assertEquals(.7680, cm[1][1], DELTA);
+		assertEquals(.0348, cm[2][1], DELTA);
+		assertEquals(.0348, cm[3][1], DELTA);
+		assertEquals(.0252, cm[4][1], DELTA);
+		assertEquals(.0286, cm[5][1], DELTA);
+		
+		assertEquals(.0353, cm[0][2], DELTA);
+		assertEquals(.0348, cm[1][2], DELTA);
+		assertEquals(.7661, cm[2][2], DELTA);
+		assertEquals(.1087, cm[3][2], DELTA);
+		assertEquals(.0258, cm[4][2], DELTA);
+		assertEquals(.0293, cm[5][2], DELTA);
+		
+		assertEquals(.0353, cm[0][3], DELTA);
+		assertEquals(.0348, cm[1][3], DELTA);
+		assertEquals(.1087, cm[2][3], DELTA);
+		assertEquals(.7661, cm[3][3], DELTA);
+		assertEquals(.0258, cm[4][3], DELTA);
+		assertEquals(.0293, cm[5][3], DELTA);
+		
+		assertEquals(.0517, cm[0][4], DELTA);
+		assertEquals(.0503, cm[1][4], DELTA);
+		assertEquals(.0517, cm[2][4], DELTA);
+		assertEquals(.0517, cm[3][4], DELTA);
+		assertEquals(.6094, cm[4][4], DELTA);
+		assertEquals(.1851, cm[5][4], DELTA);
+		
+		assertEquals(.0586, cm[0][5], DELTA);
+		assertEquals(.0571, cm[1][5], DELTA);
+		assertEquals(.0586, cm[2][5], DELTA);
+		assertEquals(.0586, cm[3][5], DELTA);
+		assertEquals(.1851, cm[4][5], DELTA);
+		assertEquals(.5818, cm[5][5], DELTA);
+	}
+	
+	@Test
+	public void testPcc() throws EvaluationException {
+		
+		float pcc = evaluationExact.getEvidenceSetPCC();
+		
+		// Pcc = 70.95
+		assertEquals(.7095, pcc, DELTA);
+	}
+	
+}
