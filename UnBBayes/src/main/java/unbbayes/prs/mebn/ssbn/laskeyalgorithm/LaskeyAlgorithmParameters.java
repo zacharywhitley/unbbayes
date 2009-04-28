c33f73d0ceef06ee9ab40d0050cd394eb191b397
laecio_lima <laecio_lima@edda2279-5339-4143-a38b-a3a7c4ae352d>
2009-04-28 03:48:05 +0000

    Implementation of the laskey'n ssbn generation algorithm (not finish yet)

--- UnBBayes/src/main/java/unbbayes/prs/mebn/ssbn/laskeyalgorithm/LaskeyAlgorithmParameters.java
+++ UnBBayes/src/main/java/unbbayes/prs/mebn/ssbn/laskeyalgorithm/LaskeyAlgorithmParameters.java
@@ -12,8 +12,8 @@ public class LaskeyAlgorithmParameters extends Parameters{
 	public static final int RECURSIVE_LIMIT       = 0x0001;
 	
 	//Select the parts of the algorithm to execute
-	public static final int DO_BUILDER            = 0x0002; 
-	public static final int DO_PRUNE              = 0x0003; 
+	public static final int DO_BUILDER            = 0x0002;
+	public static final int DO_PRUNE              = 0x0003;
 	public static final int DO_CPT_GENERATION     = 0x0004;
 	
 	public static final int USE_USER_INTERATION   = 0x0005; 
