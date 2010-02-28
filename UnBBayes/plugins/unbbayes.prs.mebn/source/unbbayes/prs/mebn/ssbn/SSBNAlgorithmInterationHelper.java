d6aaca99929e17f5255a336ede2799f0427fb717
cardialfly <cardialfly@edda2279-5339-4143-a38b-a3a7c4ae352d>
2010-02-28 18:18:26 +0000

    Removing plugin resources, since they are separated maven projects now.

--- UnBBayes/plugins/unbbayes.prs.mebn/source/unbbayes/prs/mebn/ssbn/SSBNAlgorithmInterationHelper.java
+++
@@ -1,9 +0,0 @@
-package unbbayes.prs.mebn.ssbn;
-
-import unbbayes.prs.mebn.OrdinaryVariable;
-
-public interface SSBNAlgorithmInterationHelper{
-	
-	public OVInstance getInstanceValueForOVFault(OrdinaryVariable ov);  
-	
-}
