e8ff44818346b928db298afcb9658d3f2834bf40
cardialfly <cardialfly@edda2279-5339-4143-a38b-a3a7c4ae352d>
2011-05-14 19:24:21 +0000

    Creating classes to represent a KB in PR-OWL2 format. I've chosen this approach because I don't want to disable PR-OWL1 in OWL2 completely.

---
+++ unbbayes.gui.mebn.ontology.protege/src/main/java/unbbayes/prs/mebn/kb/extension/ontology/protege/PROWL2KnowledgeBaseBuilder.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package unbbayes.prs.mebn.kb.extension.ontology.protege;
+
+import unbbayes.controller.mebn.IMEBNMediator;
+import unbbayes.prs.mebn.MultiEntityBayesianNetwork;
+import unbbayes.prs.mebn.kb.KnowledgeBase;
+
+
+/**
+ * This builder creates a {@link unbbayes.prs.mebn.kb.KnowledgeBase} which is an adaptor to {@link org.semanticweb.owlapi.reasoner.OWLReasoner},
+ * but it is adapted for PROWL2-specific routines.
+ * It overwrites {@link #buildKB(MultiEntityBayesianNetwork, IMEBNMediator)} so that
+ * it returns an instance of {@link PROWL2KnowledgeBase}.
+ * @author Shou Matsumoto
+ *
+ */
+public class PROWL2KnowledgeBaseBuilder extends OWL2KnowledgeBaseBuilder {
+
+	/**
+	 * Default constructor must be public for plug-in support
+	 */
+	public PROWL2KnowledgeBaseBuilder() {
+		super();
+		this.setName("DL Reasoner for PR-OWL2");
+	}
+
+	/**
+	 * @see unbbayes.prs.mebn.kb.extension.ontology.protege.OWL2KnowledgeBaseBuilder#buildKB(unbbayes.prs.mebn.MultiEntityBayesianNetwork, unbbayes.controller.mebn.IMEBNMediator)
+	 */
+	public KnowledgeBase buildKB(MultiEntityBayesianNetwork mebn,
+			IMEBNMediator mediator) throws InstantiationException {
+		return PROWL2KnowledgeBase.getInstance(mebn, mediator);
+	}
+
+}
