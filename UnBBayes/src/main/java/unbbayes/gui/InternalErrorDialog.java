c570dfb49bd89b6692a4c390db78e9245fcf1117
cardialfly <cardialfly@edda2279-5339-4143-a38b-a3a7c4ae352d>
2011-05-15 16:24:22 +0000

    Fixed a bug that happens in resources when this class is loaded from plug-ins

--- UnBBayes/src/main/java/unbbayes/gui/InternalErrorDialog.java
+++ UnBBayes/src/main/java/unbbayes/gui/InternalErrorDialog.java
@@ -20,17 +20,16 @@
  */
 package unbbayes.gui;
 
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 import javax.swing.JOptionPane;
 
 /**
- * Janela que informa para o usu�rio que ocorreu um erro interno. 
- * Este painel � mostrado quando ocorre um erro que n�o deveria ocorrer: 
- * alguma condi��o que foi aceita como verdadeira e na execu��o se mostrou falsa. 
- * Possibilita que o usu�rio envie para os desenvolvedores um e-mail com a 
- * descri��o do erro para poss�vel corre��o. 
- * 
+ * This dialog indicates that there was an internal error.
+ * For example, when a condition assumed to be true was in fact false.
+ * This dialog may also be used in order to e-mail notifications to developers
+ * with a description of the error.
  * @author Laecio Lima dos Santos
  * @version 1.0 06/05/07
  *
@@ -39,9 +38,11 @@ public class InternalErrorDialog {
 
 	/** Load resource file from this package */
   	private static ResourceBundle resource = unbbayes.util.ResourceController.newInstance().getBundle(
-  			unbbayes.gui.resources.GuiResources.class.getName());
+  			unbbayes.gui.resources.GuiResources.class.getName(),
+  			Locale.getDefault(),
+  			InternalErrorDialog.class.getClassLoader());
 	
-	//TODO concluir o painel, colocando op��es para reporte do erro... 
+	//TODO conclude the panel, inserting options for error report... 
 	public InternalErrorDialog(){
 		JOptionPane.showMessageDialog(
 				null, 
