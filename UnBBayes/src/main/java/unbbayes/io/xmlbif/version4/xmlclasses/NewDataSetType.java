58e73cc72cd04a920f36b5fec9c99868a3445938
bitocampo <bitocampo@edda2279-5339-4143-a38b-a3a7c4ae352d>
2012-08-09 17:19:16 +0000

    This tag has been created before another programmer (bitocampo) start developing.

--- UnBBayes/src/main/java/unbbayes/io/xmlbif/version4/xmlclasses/NewDataSetType.java
+++
@@ -1,8 +0,0 @@
-package unbbayes.io.xmlbif.version4.xmlclasses;
-
-public interface NewDataSetType {
-  public unbbayes.io.xmlbif.version4.xmlclasses.BIFType getBIF();
-
-  public void setBIF(unbbayes.io.xmlbif.version4.xmlclasses.BIFType pBIF);
-
-}
--- UnBBayesServerEJBProject/ejbModule/unbbs/manipulation/ModelManipulation.java
+++
@@ -1,7 +0,0 @@
-package unbbs.manipulation;
-/**
- * Remote interface for Enterprise Bean: ModelManipulation
- */
-public interface ModelManipulation extends javax.ejb.EJBObject {
-	public String compile(int modelId) throws java.rmi.RemoteException;
-}
