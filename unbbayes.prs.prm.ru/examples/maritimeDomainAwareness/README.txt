70a8d17ea182cf3925e16bec05a164a3cfa0c509
bitocampo <bitocampo@edda2279-5339-4143-a38b-a3a7c4ae352d>
2012-10-06 21:11:56 +0000

    new tables and attributes has been added for mda example.

--- unbbayes.prs.prm.ru/examples/maritimeDomainAwareness/README.txt
+++ unbbayes.prs.prm.ru/examples/maritimeDomainAwareness/README.txt
@@ -1,5 +1,5 @@
 
-This is a modification of the study case named Moaritime Domain Awareness
+This is a modification of the study case named Maritime Domain Awareness
 [Carvalho, 2011]. 
 
 
--- unbbayes.prs.prm.ru/examples/maritimeDomainAwareness/creationScript.SQL.sql
+++ unbbayes.prs.prm.ru/examples/maritimeDomainAwareness/creationScript.SQL.sql
@@ -1,26 +1,6 @@
--- Database schema -----
-	
-ALTER TABLE `ELECTRONIC_EQUIPEMENT` ADD FOREIGN KEY (`owner`) REFERENCES `MDA`.`SHIP`(`id`) ;
-
-
-ALTER TABLE `PERSON` ADD FOREIGN KEY (`relatedTo`) REFERENCES `MDA`.`PERSON`(`id`) ;
-ALTER TABLE `PERSON` ADD FOREIGN KEY (`organization`) REFERENCES `MDA`.`ORGANIZATION`(`id`) ;
-ALTER TABLE `PERSON` ADD FOREIGN KEY (`crewMember`) REFERENCES `MDA`.`SHIP`(`id`) ;
-
-ALTER TABLE `MEETING` ADD FOREIGN KEY (`ship1`) REFERENCES `MDA`.`SHIP`(`id`) ;
-ALTER TABLE `MEETING` ADD FOREIGN KEY (`ship2`) REFERENCES `MDA`.`SHIP`(`id`) ;
-	
-
-
-	
--- phpMyAdmin SQL Dump
--- version 3.4.10.1deb1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Oct 05, 2012 at 11:19 AM
--- Server version: 5.5.24
--- PHP Version: 5.3.10-1ubuntu3.4
+-- MDA STUDY CASE
+-- DB CREATION SCRIPT.
+-- Generation Time: Oct 06, 2012 at 05:40 PM
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -38,17 +18,31 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `ELECTRONIC_EQUIPEMENT`
+-- Table structure for table `ELECTRONIC_EQUIPMENT`
 --
 
-CREATE TABLE IF NOT EXISTS `ELECTRONIC_EQUIPEMENT` (
+CREATE TABLE IF NOT EXISTS `ELECTRONIC_EQUIPMENT` (
   `id` int(11) NOT NULL,
   `isWorking` varchar(1) DEFAULT NULL,
   `isResponsive` varchar(1) DEFAULT NULL,
-  `type` varchar(30) DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
   `owner` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `owner` (`owner`),
+  KEY `type` (`type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `EQUIPMENT_TYPE`
+--
+
+CREATE TABLE IF NOT EXISTS `EQUIPMENT_TYPE` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -58,7 +52,9 @@ CREATE TABLE IF NOT EXISTS `ELECTRONIC_EQUIPEMENT` (
 
 CREATE TABLE IF NOT EXISTS `MEETING` (
   `ship1` int(11) DEFAULT NULL,
-  `ship2` int(11) DEFAULT NULL
+  `ship2` int(11) DEFAULT NULL,
+  KEY `ship1` (`ship1`),
+  KEY `ship2` (`ship2`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -68,8 +64,9 @@ CREATE TABLE IF NOT EXISTS `MEETING` (
 --
 
 CREATE TABLE IF NOT EXISTS `ORGANIZATION` (
-  `id` varchar(30) NOT NULL,
-  `terrorist` varchar(1) DEFAULT NULL,
+  `id` int(30) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `isTerrorist` varchar(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -82,11 +79,22 @@ CREATE TABLE IF NOT EXISTS `ORGANIZATION` (
 CREATE TABLE IF NOT EXISTS `PERSON` (
   `id` varchar(30) NOT NULL,
   `relatedTo` varchar(30) DEFAULT NULL,
-  `terrorist` varchar(1) DEFAULT NULL,
-  `organization` varchar(30) DEFAULT NULL,
+  `isTerrorist` varchar(1) DEFAULT NULL,
+  `organization` int(11) DEFAULT NULL,
   `crewMember` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `relatedTo` (`relatedTo`),
+  KEY `organization` (`organization`),
+  KEY `crewMember` (`crewMember`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `PERSON`
+--
+
+INSERT INTO `PERSON` (`id`, `relatedTo`, `isTerrorist`, `organization`, `crewMember`) VALUES
+('Terrorist', NULL, 'T', NULL, NULL),
+('TerroristRelated', 'Terrorist', 'F', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -96,11 +104,56 @@ CREATE TABLE IF NOT EXISTS `PERSON` (
 
 CREATE TABLE IF NOT EXISTS `SHIP` (
   `id` int(11) NOT NULL,
-  `interested` varchar(1) DEFAULT NULL,
-  `typeOfShip` varchar(30) DEFAULT NULL,
-  `ecmDeployed` varchar(1) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `isOfInterest` varchar(1) DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
+  `isECMDeployed` varchar(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `type` (`type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `SHIP_TYPE`
+--
+
+CREATE TABLE IF NOT EXISTS `SHIP_TYPE` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `ELECTRONIC_EQUIPMENT`
+--
+ALTER TABLE `ELECTRONIC_EQUIPMENT`
+  ADD CONSTRAINT `ELECTRONIC_EQUIPMENT_ibfk_2` FOREIGN KEY (`type`) REFERENCES `EQUIPMENT_TYPE` (`id`),
+  ADD CONSTRAINT `ELECTRONIC_EQUIPMENT_ibfk_1` FOREIGN KEY (`owner`) REFERENCES `SHIP` (`id`);
+
+--
+-- Constraints for table `MEETING`
+--
+ALTER TABLE `MEETING`
+  ADD CONSTRAINT `MEETING_ibfk_1` FOREIGN KEY (`ship1`) REFERENCES `SHIP` (`id`),
+  ADD CONSTRAINT `MEETING_ibfk_10` FOREIGN KEY (`ship2`) REFERENCES `SHIP` (`id`);
+
+--
+-- Constraints for table `PERSON`
+--
+ALTER TABLE `PERSON`
+  ADD CONSTRAINT `PERSON_ibfk_9` FOREIGN KEY (`crewMember`) REFERENCES `SHIP` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
+  ADD CONSTRAINT `PERSON_ibfk_1` FOREIGN KEY (`relatedTo`) REFERENCES `PERSON` (`id`),
+  ADD CONSTRAINT `PERSON_ibfk_8` FOREIGN KEY (`organization`) REFERENCES `ORGANIZATION` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
+
+--
+-- Constraints for table `SHIP`
+--
+ALTER TABLE `SHIP`
+  ADD CONSTRAINT `SHIP_ibfk_1` FOREIGN KEY (`type`) REFERENCES `SHIP_TYPE` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
