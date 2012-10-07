0791f7dc6f8c4476136a9d518d18e5480a47f345
cardialfly <cardialfly@edda2279-5339-4143-a38b-a3a7c4ae352d>
2012-10-07 19:27:41 +0000

    Changed the normalize method of potential table to set to uniform distribution if all potentials are zero.

--- UnBBayes/src/main/java/unbbayes/prs/bn/PotentialTable.java
+++ UnBBayes/src/main/java/unbbayes/prs/bn/PotentialTable.java
@@ -967,23 +967,31 @@ public abstract class PotentialTable implements Cloneable, java.io.Serializable,
         float n = 0;
         float valor;
 
-        int sizeDados = this.tableSize();
-        for (int c = 0; c < sizeDados; c++) {
+        int dataSize = this.tableSize();
+        for (int c = 0; c < dataSize; c++) {
             n += this.getValue(c);
         }
-        if (Math.abs(n - 1.0) > 0.001) {
-            for (int c = 0; c < sizeDados; c++) {
-                valor = this.getValue(c);
-                if (valor == 0.0) {
-                	// zeros will remain always zero.
-                	continue;
-                }
-                if (n == 0.0) {
-                    throw new IllegalStateException(resource.getString("InconsistencyUnderflowException"));
-                }
-                valor /= n;
-                this.setValue(c, valor);
-            }
+        if (n > 0.00005) {// if precision is 4 digits, error margin is half of precision (i.e. 0.00005)
+        	if (Math.abs(n - 1.0) > 0.00005) {	// if precision is 4 digits, error margin is half of precision (i.e. 0.00005)
+        		for (int c = 0; c < dataSize; c++) {
+        			valor = this.getValue(c);
+        			if (valor == 0.0) {
+        				// zeros will remain always zero.
+        				continue;
+        			}
+        			if (n == 0.0) {
+        				throw new IllegalStateException(resource.getString("InconsistencyUnderflowException"));
+        			}
+        			valor /= n;
+        			this.setValue(c, valor);
+        		}
+    		}
+        } else {
+        	// this is an impossible state (e.g. all clique potentials are zero).
+        	// by default, if all clique potentials have the same value (zero, though), they will be normalized to uniform distribution.
+        	 for (int c = 0; c < dataSize; c++) {
+        		 this.setValue(c, 1f/dataSize);
+        	 }
         }
         return n;
     }
