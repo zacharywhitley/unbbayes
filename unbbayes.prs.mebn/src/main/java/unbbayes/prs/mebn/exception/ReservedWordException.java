63074c84eff4cffec9b557f342131cab588d6263
bitocampo <bitocampo@edda2279-5339-4143-a38b-a3a7c4ae352d>
2012-11-25 18:43:20 +0000

    Tag before merging

--- unbbayes.prs.mebn/src/main/java/unbbayes/prs/mebn/exception/ReservedWordException.java
+++
@@ -1,9 +0,0 @@
-package unbbayes.prs.mebn.exception;
-
-public class ReservedWordException extends NameException{
-
-	public ReservedWordException(String name){
-		super("", name); 
-	}
-	
-}
--- unbbayes.prs.prm.ru/src/main/java/unbbayes/prm/model/AggregateFunctionName.java
+++
@@ -1,5 +0,0 @@
-package unbbayes.prm.model;
-
-public enum AggregateFunctionName {
-	mode, min, max, mean, median,
-}
