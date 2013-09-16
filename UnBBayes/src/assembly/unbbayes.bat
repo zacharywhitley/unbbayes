f13c62737624d855eb34e75b8952fde8333401eb
cardialfly <cardialfly@edda2279-5339-4143-a38b-a3a7c4ae352d>
2013-09-16 23:02:09 +0000

    Changing the batch and shell scripts to point to version 4.13.6 now.

--- UnBBayes/src/assembly/unbbayes.bat
+++ UnBBayes/src/assembly/unbbayes.bat
@@ -1,2 +1,2 @@
-java -jar -Xms128M -Xmx1024M -Djavax.xml.transform.TransformerFactory=unbbayes.util.XalanIndentNumberBugFixer unbbayes-4.13.5.jar
+java -jar -Xms128M -Xmx1024M -Djavax.xml.transform.TransformerFactory=unbbayes.util.XalanIndentNumberBugFixer unbbayes-4.13.6.jar
 
--- UnBBayes/src/assembly/unbbayes.sh
+++ UnBBayes/src/assembly/unbbayes.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -jar -Xms128M -Xmx1024M -Djavax.xml.transform.TransformerFactory=unbbayes.util.XalanIndentNumberBugFixer unbbayes-4.13.5.jar
+java -jar -Xms128M -Xmx1024M -Djavax.xml.transform.TransformerFactory=unbbayes.util.XalanIndentNumberBugFixer unbbayes-4.13.6.jar
 
