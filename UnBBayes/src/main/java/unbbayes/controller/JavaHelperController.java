766dc99b0dbce12ffaef26434219b04e22d663ff
laecio_lima <laecio_lima@edda2279-5339-4143-a38b-a3a7c4ae352d>
2013-11-27 10:54:42 +0000

    refactory

--- UnBBayes/src/main/java/unbbayes/controller/JavaHelperController.java
+++ UnBBayes/src/main/java/unbbayes/controller/JavaHelperController.java
@@ -10,15 +10,14 @@ import javax.swing.JFrame;
 
 /**
  * Controller for Java Helper
- * 
- * @author Laecio
  */
 public class JavaHelperController {
 
 	private static JavaHelperController singleton; 
 	
 	private ResourceBundle resource = 
-		unbbayes.util.ResourceController.newInstance().getBundle(unbbayes.controller.resources.ControllerResources.class.getName());
+		unbbayes.util.ResourceController.newInstance().getBundle(
+				unbbayes.controller.resources.ControllerResources.class.getName());
 	
 	public static JavaHelperController getInstance(){
 		if(singleton == null){
@@ -35,39 +34,59 @@ public class JavaHelperController {
 	 */
 	public void openHelp(Component component) throws Exception
 	{	 component.setCursor(new Cursor(Cursor.WAIT_CURSOR));
+	
 		String className = component.getClass().getName();
 		HelpSet set = null;
-		if (className.equals("unbbayes.datamining.gui.InvokerMain"))
-		{	 set = new HelpSet(null, getClass().getResource("/help/DataMiningHelp/Data_Mining.hs"));
+		boolean find = false; 
+		
+		if (className.equals("unbbayes.datamining.gui.InvokerMain")){	 
+			set = new HelpSet(null, getClass().getResource("/help/DataMiningHelp/Data_Mining.hs"));
+			find = true; 
 		}
-		else if (className.equals("unbbayes.datamining.gui.id3.DecisionTreeMain"))
-		{	 set = new HelpSet(null, getClass().getResource("/help/DataMiningHelp/Decision_Tree.hs"));
+		
+		if (className.equals("unbbayes.datamining.gui.id3.DecisionTreeMain")){	 
+			set = new HelpSet(null, getClass().getResource("/help/DataMiningHelp/Decision_Tree.hs"));
+			find = true; 
 		}
-				else if (className.equals("unbbayes.datamining.gui.c45.DecisionTreeMain"))
-				{	 set = new HelpSet(null, getClass().getResource("/help/C45Help/C45.hs"));
-				}
-				else if (className.equals("unbbayes.datamining.gui.evaluation.EvaluationMain"))
-		{	 set = new HelpSet(null, getClass().getResource("/help/DataMiningHelp/Evaluation.hs"));
+		
+		if (className.equals("unbbayes.datamining.gui.c45.DecisionTreeMain")){	 
+			set = new HelpSet(null, getClass().getResource("/help/C45Help/C45.hs"));
+			find = true; 
 		}
-		else if (className.equals("unbbayes.datamining.gui.naivebayes.NaiveBayesMain"))
-		{	 set = new HelpSet(null, getClass().getResource("/help/DataMiningHelp/Naive_Bayes.hs"));
+		
+		if (className.equals("unbbayes.datamining.gui.evaluation.EvaluationMain")){	 
+			set = new HelpSet(null, getClass().getResource("/help/DataMiningHelp/Evaluation.hs"));
+			find = true; 
 		}
-		else if (className.equals("unbbayes.datamining.gui.preprocessor.PreprocessorMain"))
-		{	 set = new HelpSet(null, getClass().getResource("/help/DataMiningHelp/Preprocessor.hs"));
+		
+		if (className.equals("unbbayes.datamining.gui.naivebayes.NaiveBayesMain")){	 
+			set = new HelpSet(null, getClass().getResource("/help/DataMiningHelp/Naive_Bayes.hs"));
+			find = true; 
 		}
-		else if (className.equals("unbbayes.datamining.gui.neuralmodel.NeuralModelMain"))
-		{	 set = new HelpSet(null, getClass().getResource("/help/CNMHelp/cnm.hs"));
+		
+		if (className.equals("unbbayes.datamining.gui.preprocessor.PreprocessorMain")){	 
+			set = new HelpSet(null, getClass().getResource("/help/DataMiningHelp/Preprocessor.hs"));
+			find = true; 
 		}
-				else if (className.equals("unbbayes.datamining.gui.neuralnetwork.NeuralNetworkMain"))
-				{	 set = new HelpSet(null, getClass().getResource("/help/BpnHelp/BpnHelp.hs"));
-				}
-				else if (className.equals("unbbayes.gui.UnBBayesFrame"))
-				{	 set = new HelpSet(null, getClass().getResource("/help/JUnBBayes.hs"));
-				}
-		else
-		{
-					component.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
-					throw new Exception("HelpSet not found "+this.getClass().getName());
+		
+		if (className.equals("unbbayes.datamining.gui.neuralmodel.NeuralModelMain")){	 
+			set = new HelpSet(null, getClass().getResource("/help/CNMHelp/cnm.hs"));
+			find = true; 
+		}
+		
+		if (className.equals("unbbayes.datamining.gui.neuralnetwork.NeuralNetworkMain")){	 
+			set = new HelpSet(null, getClass().getResource("/help/BpnHelp/BpnHelp.hs"));
+			find = true; 
+		}
+		
+		if (className.equals("unbbayes.gui.UnBBayesFrame")){	 
+			set = new HelpSet(null, getClass().getResource("/help/JUnBBayes.hs"));
+			find = true; 
+		}
+		
+		if (!find){
+			component.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			throw new Exception("HelpSet not found "+this.getClass().getName());
 		}
 
 		JHelp help = new JHelp(set);
