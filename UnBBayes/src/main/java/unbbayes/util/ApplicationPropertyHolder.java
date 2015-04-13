164a418456748654331d300ecf82ec691a61ac7f
cardialfly <cardialfly@edda2279-5339-4143-a38b-a3a7c4ae352d>
2015-04-13 21:58:51 +0000

    ApplicationPropertyHolder will also look for application.properties file in root folder.

--- UnBBayes/src/main/java/unbbayes/util/ApplicationPropertyHolder.java
+++ UnBBayes/src/main/java/unbbayes/util/ApplicationPropertyHolder.java
@@ -9,8 +9,10 @@ import java.util.Properties;
 /**
  * @author Shou Matsumoto
  * A static holder for application.property file.
- * The default place to look for is a file named "application.property"
- * at classloader path. If not found, it will also search the src/main/resources
+ * The default place to look for is a file named "application.property" at application's root folder. 
+ * If not found, it will also search at the classloader path.
+ * If not found, it will also search the src/main/resources,
+ * and if that fails too, it will search for src/.
  */
 public class ApplicationPropertyHolder implements
 		Serializable {
@@ -26,14 +28,25 @@ public class ApplicationPropertyHolder implements
 		// initialize property using default values
 		property = new Properties();
 		try {
-			property.load(ApplicationPropertyHolder.class.getClassLoader().getResourceAsStream(applicationPropertyPath));
+			property.load(ApplicationPropertyHolder.class.getClassLoader().getResourceAsStream(new java.io.File(applicationPropertyPath).toURI().getPath()));
 		} catch (Exception e) {
 			try {
-				applicationPropertyPath = "src/main/resources/" + applicationPropertyPath;
 				property.load(ApplicationPropertyHolder.class.getClassLoader().getResourceAsStream(applicationPropertyPath));
 			} catch (Exception e2) {
-				e.printStackTrace();
-				e2.printStackTrace();
+				try {
+					applicationPropertyPath = "src/main/resources/" + applicationPropertyPath;
+					property.load(ApplicationPropertyHolder.class.getClassLoader().getResourceAsStream(applicationPropertyPath));
+				} catch (Exception e3) {
+					try {
+						applicationPropertyPath = "src/" + applicationPropertyPath;
+						property.load(ApplicationPropertyHolder.class.getClassLoader().getResourceAsStream(applicationPropertyPath));
+					} catch (Exception e4) {
+						e.printStackTrace();
+						e2.printStackTrace();
+						e3.printStackTrace();
+						e4.printStackTrace();
+					}
+				}
 			}
 		}
 	}
