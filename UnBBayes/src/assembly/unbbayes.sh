e2a3613722c6c08655efa35edb181f1c2521190a
cardialfly <cardialfly@edda2279-5339-4143-a38b-a3a7c4ae352d>
2016-02-19 23:45:24 +0000

    Changing version number to 4.21.18

--- UnBBayes/src/assembly/unbbayes.sh
+++ UnBBayes/src/assembly/unbbayes.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -jar -Xms128M -Xmx1024M -Djavax.xml.transform.TransformerFactory=unbbayes.util.XalanIndentNumberBugFixer unbbayes-4.21.15.jar
+java -jar -Xms128M -Xmx1024M -Djavax.xml.transform.TransformerFactory=unbbayes.util.XalanIndentNumberBugFixer unbbayes-4.21.18.jar
 
