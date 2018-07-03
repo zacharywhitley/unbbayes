6a176e34f7d0367f57822a6a603c217aa62900d0
cardialfly <cardialfly@edda2279-5339-4143-a38b-a3a7c4ae352d>
2018-07-03 03:28:47 +0000

    Added test cases to check that hierarchy of types are properly loaded to MEBN object in PR_OWL 2 (this is different from hierarchy of types being handled by KB and reasoner -- this new change will reflect visibility by GUI).

---
+++ unbbayes.gui.mebn.ontology.protege/src/test/java/unbbayes/io/mebn/TestOWLAPICompatiblePROWL2IO.java
@@ -0,0 +1,171 @@
+/**
+ * 
+ */
+package unbbayes.io.mebn;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+import unbbayes.io.exception.LoadException;
+import unbbayes.io.mebn.owlapi.OWLAPICompatiblePROWL2IO;
+import unbbayes.prs.mebn.MultiEntityBayesianNetwork;
+import unbbayes.prs.mebn.entity.ObjectEntity;
+
+/**
+ * @author MImran
+ *
+ */
+public class TestOWLAPICompatiblePROWL2IO extends TestCase {
+	
+	private OWLAPICompatiblePROWL2IO io = null;
+
+	/**
+	 * @param name
+	 */
+	public TestOWLAPICompatiblePROWL2IO(String name) {
+		super(name);
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+		io = (OWLAPICompatiblePROWL2IO) OWLAPICompatiblePROWL2IO.newInstance();
+		assertNotNull(io);
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	/**
+	 * Test method for {@link unbbayes.io.mebn.owlapi.OWLAPICompatiblePROWL2IO#loadObjectEntity(org.semanticweb.owlapi.model.OWLOntology, unbbayes.prs.mebn.MultiEntityBayesianNetwork)}
+	 * to see if the hierarchy of object entities are properly loaded.
+	 * @throws URISyntaxException 
+	 * @throws IOException 
+	 * @throws LoadException 
+	 */
+	public void testLoadObjectEntityHierarchy() throws LoadException, IOException, URISyntaxException {
+		MultiEntityBayesianNetwork mebn = (MultiEntityBayesianNetwork) io.load(new File(getClass().getResource("/VehicleIdentificationTBox/VehicleIdentificationTBoxHierarchySample.owl").toURI()));
+		
+		// check those entities with no parent/children
+		ObjectEntity entity = mebn.getObjectEntityContainer().getObjectEntityByName("Report");
+		assertNotNull(entity);
+		List<ObjectEntity> parents = mebn.getObjectEntityContainer().getParentsOfObjectEntity(entity);
+		assertNotNull(parents);
+		assertEquals(1, parents.size());
+		assertTrue(parents.contains(mebn.getObjectEntityContainer().getRootObjectEntity()));
+		List<ObjectEntity> children = mebn.getObjectEntityContainer().getChildrenOfObjectEntity(entity);
+		assertNotNull(children);
+		assertEquals(0, children.size());
+		
+		entity = mebn.getObjectEntityContainer().getObjectEntityByName("TimeStep");
+		assertNotNull(entity);
+		parents = mebn.getObjectEntityContainer().getParentsOfObjectEntity(entity);
+		assertNotNull(parents);
+		assertEquals(1, parents.size());
+		assertTrue(parents.contains(mebn.getObjectEntityContainer().getRootObjectEntity()));
+		children = mebn.getObjectEntityContainer().getChildrenOfObjectEntity(entity);
+		assertNotNull(children);
+		assertEquals(0, children.size());
+		
+		entity = mebn.getObjectEntityContainer().getObjectEntityByName("Region");
+		assertNotNull(entity);
+		parents = mebn.getObjectEntityContainer().getParentsOfObjectEntity(entity);
+		assertNotNull(parents);
+		assertEquals(1, parents.size());
+		assertTrue(parents.contains(mebn.getObjectEntityContainer().getRootObjectEntity()));
+		children = mebn.getObjectEntityContainer().getChildrenOfObjectEntity(entity);
+		assertNotNull(children);
+		assertEquals(0, children.size());
+		
+		
+		// check object entity with children
+		
+		ObjectEntity objectEntity = mebn.getObjectEntityContainer().getObjectEntityByName("Object");
+		assertNotNull(objectEntity);
+		ObjectEntity vehicleEntity = mebn.getObjectEntityContainer().getObjectEntityByName("Vehicle");
+		assertNotNull(vehicleEntity);
+		ObjectEntity trackedVehicleEntity = mebn.getObjectEntityContainer().getObjectEntityByName("TrackedVehicle");
+		assertNotNull(trackedVehicleEntity);
+		ObjectEntity wheeledVehicleEntity = mebn.getObjectEntityContainer().getObjectEntityByName("WheeledVehicle");
+		assertNotNull(wheeledVehicleEntity);
+		
+		entity = objectEntity;
+		parents = mebn.getObjectEntityContainer().getParentsOfObjectEntity(entity);
+		assertNotNull(parents);
+		assertEquals(1, parents.size());
+		assertTrue(parents.contains(mebn.getObjectEntityContainer().getRootObjectEntity()));
+		
+		children = mebn.getObjectEntityContainer().getChildrenOfObjectEntity(entity);
+		assertNotNull(children);
+		assertEquals(1, children.size());
+		assertTrue(children.contains(vehicleEntity));
+		
+		entity = vehicleEntity;
+		parents = mebn.getObjectEntityContainer().getParentsOfObjectEntity(entity);
+		assertNotNull(parents);
+		assertEquals(1, parents.size());
+		assertTrue(parents.contains(objectEntity));
+		
+		children = mebn.getObjectEntityContainer().getChildrenOfObjectEntity(entity);
+		assertNotNull(children);
+		assertEquals(2, children.size());
+		assertTrue(children.contains(trackedVehicleEntity));
+		assertTrue(children.contains(wheeledVehicleEntity));
+		
+		entity = trackedVehicleEntity;
+		parents = mebn.getObjectEntityContainer().getParentsOfObjectEntity(entity);
+		assertNotNull(parents);
+		assertEquals(1, parents.size());
+		assertTrue(parents.contains(vehicleEntity));
+		
+		children = mebn.getObjectEntityContainer().getChildrenOfObjectEntity(entity);
+		assertNotNull(children);
+		assertEquals(0, children.size());
+		
+		entity = wheeledVehicleEntity;
+		parents = mebn.getObjectEntityContainer().getParentsOfObjectEntity(entity);
+		assertNotNull(parents);
+		assertEquals(1, parents.size());
+		assertTrue(parents.contains(vehicleEntity));
+		
+		children = mebn.getObjectEntityContainer().getChildrenOfObjectEntity(entity);
+		assertNotNull(children);
+		assertEquals(0, children.size());
+		
+		
+	}
+	
+
+	/**
+	 * New versions of eclipse seem to cause conflicts between multi-thread instances of protege/owlapi
+	 * when executing JUnit.
+	 * Please, run this suite (see {@link #main(String[])}) in case you find such problems.
+	 * @return
+	 */
+	public static Test suite() {
+		TestSuite suite = new TestSuite(TestOWLAPICompatiblePROWL2IO.class.getName());
+		//$JUnit-BEGIN$
+		suite.addTestSuite(TestOWLAPICompatiblePROWL2IO.class);
+		//$JUnit-END$
+		return suite;
+	}
+	
+	public static void main(String[] args) {
+		TestRunner runner = new TestRunner(System.out);
+		runner.run(suite());
+		System.exit(0);
+	}
+
+}
