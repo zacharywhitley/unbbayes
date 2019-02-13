11ee194202def2768b387c798b645c02df20d345
cardialfly <cardialfly@edda2279-5339-4143-a38b-a3a7c4ae352d>
2019-02-13 10:46:36 +0000

    [Based on Bo Yu's patch] Project refactoring in order to include NetIO with support for Dirichlet parameters, and a Junction Tree algorithm with monte carlo sampling in order to sample CPTs from such Dirichlet distribution and run classic junction tree in order to calculate multiple marginal probabilities and their mean/standard deviations.

---
+++ UnBBayes/src/main/java/unbbayes/io/CountCompatibleNetIO.java
@@ -0,0 +1,165 @@
+package unbbayes.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import unbbayes.io.NetIO;
+import unbbayes.io.exception.LoadException;
+import unbbayes.prs.Node;
+import unbbayes.prs.bn.IRandomVariable;
+import unbbayes.prs.bn.PotentialTable;
+import unbbayes.prs.bn.ProbabilisticNetwork;
+import unbbayes.prs.bn.ProbabilisticNode;
+import unbbayes.prs.bn.ProbabilisticTable;
+import unbbayes.prs.bn.SingleEntityNetwork;
+import unbbayes.prs.bn.cpt.impl.NormalizeTableFunction;
+import unbbayes.prs.builder.IProbabilisticNetworkBuilder;
+import unbbayes.prs.hybridbn.ContinuousNode;
+import unbbayes.prs.id.UtilityNode;
+
+/**
+ * This is an extension to {@link NetIO} so that
+ * parameters of Dirichlet-Multinomial distribution
+ * (counts used for parameter learning)
+ * are also properly stored and loaded in files with
+ * extension ".net".
+ * @author Shou Matsumoto
+ * TODO This is a quick and dirty version to be used as a prototype.
+ */
+public class CountCompatibleNetIO extends NetIO {
+
+	/** Prefix to be used in {@link ProbabilisticNetwork#getProperty(String)} to retrieve/store table of counts. 
+	 * The table can be retrieved with this prefix + name of the node */
+	public static final String DEFAULT_COUNT_TABLE_PREFIX = "DirichletDistributionParamTable_";
+
+	
+	/**
+	 * Method in superclass is overridden in order to store the count tables instead of
+	 * the conditional probability tables.
+	 * @see unbbayes.io.NetIO#savePotentialDeclarationBody(java.io.PrintStream, unbbayes.prs.Node, unbbayes.prs.bn.SingleEntityNetwork)
+	 */
+	@Override
+	protected void savePotentialDeclarationBody(PrintStream stream, Node node, SingleEntityNetwork net) {
+		
+		PotentialTable countTable = (PotentialTable) net.getProperty(DEFAULT_COUNT_TABLE_PREFIX + node.getName());
+		if (countTable == null) {
+			// if there is no count table to store, then reuse superclass' method
+			super.savePotentialDeclarationBody(stream, node, net);
+			return;
+		}
+		
+		if (node instanceof ContinuousNode) {
+			// just delegate to superclass
+			super.savePotentialDeclarationBody(stream, node, net);
+			return;
+		} else if (node instanceof IRandomVariable) {
+			// save the count table instead of probabilities 
+			// (probabilities can be retrieved back by normalizing the count table)
+			PotentialTable auxTable = countTable;
+			
+			// the rest of the code is the same of superclass' method
+			
+			int sizeVa1 = auxTable.variableCount();
+			
+			// stores whether this node is an utility.
+			boolean isUtilityNode = (node instanceof UtilityNode);
+
+			stream.print(" data = ");
+			
+//			TODO _if _this is an utility node, then we can simply write a space/tab separated list of utility;
+//			(it doesnt have to be hierarchical with parenthesis anymore);
+			
+			int[] coord;
+			boolean[] hasParenthesis = new boolean[sizeVa1];
+
+			int dataSize = auxTable.tableSize();
+			for (int tableCellIndex = 0; tableCellIndex < dataSize; tableCellIndex++) {	// iterating on each cell of the table
+				coord = auxTable.getMultidimensionalCoord(tableCellIndex);
+
+				int tableVarIndex = 0;		// index to be used to iterate on variables in the table
+				if (isUtilityNode 			// this is an utility node
+						&& sizeVa1 > 1 ) {	// node has 1 or more parents (sizeVa1 == 1 means no parent: i.e. the table has only 1 node -- the node itself)
+					// if this is an utility node with 1 or more parents, then utility node has only 1 state, but the table has more than 1 cell.
+					// if we add parenthesis in this scenario, each number will come between parenthesis, like "(((100) (50)) ((10) (-10))))"
+					// Since Hugin cannot open such specification, this should be converted to "((100 50) (10 -10)))"
+					tableVarIndex = 1;		// start index from 1 (ignore the 0-th variable  -- the utility node itself -- when opening the parenthesis)
+//					paren[0] = true;		// but mark utility node as visited, for compatibility
+				}
+				// include open parenthesis
+				for (; tableVarIndex < sizeVa1; tableVarIndex++) {
+					if ((coord[tableVarIndex] == 0) && (!hasParenthesis[tableVarIndex])) {
+						stream.print("(");
+						hasParenthesis[tableVarIndex] = true;
+					}
+				}
+				stream.print(" " + auxTable.getValue(tableCellIndex));
+				if ((tableCellIndex % node.getStatesSize()) == node.getStatesSize() - 1) {
+					stream.print(" ");
+				}
+
+				int cells = 1;
+
+				Node auxNode2;
+				
+				tableVarIndex = 0;	// make sure index is reset
+				// check if we should close parenthesis when this is an utility node
+				if (isUtilityNode 			// this is an utility node
+						&& sizeVa1 > 1 ) {	// node has 1 or more parents (sizeVa1 == 1 means no parent: i.e. the table has only 1 node -- the node itself)
+					tableVarIndex = 1;		// start index from 1 (ignore the 0-th variable  -- the utility node itself -- when opening the parenthesis)
+//					paren[tableVarIndex] = false;	// simply reset flag and don't add parenthesis
+				} 
+				for (; tableVarIndex < sizeVa1; tableVarIndex++) {
+					auxNode2 = (Node)auxTable.getVariableAt(tableVarIndex);
+					cells *= auxNode2.getStatesSize();
+					// check for condition to close parenthesis
+					if (((tableCellIndex + 1) % cells) == 0) {
+						// close parenthesis
+						stream.print(")");
+						if (tableVarIndex == sizeVa1 - 1) {
+							stream.print(";");
+						}
+						hasParenthesis[tableVarIndex] = false;
+					}
+				}
+
+				if (((tableCellIndex + 1) % node.getStatesSize()) == 0) {
+					stream.println();
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Method in superclass is overridden in order to load the potential data
+	 * to the count tables and then set the conditional probability tables to
+	 * the normalized values of count tables.
+	 * @see unbbayes.io.NetIO#load(java.io.File, unbbayes.prs.bn.SingleEntityNetwork, unbbayes.prs.builder.IProbabilisticNetworkBuilder)
+	 */
+	@Override
+	protected void load(File input, SingleEntityNetwork net, IProbabilisticNetworkBuilder networkBuilder) throws IOException, LoadException {
+		
+		// run the same code of superclass (this should fill the potential tables with counts)
+		super.load(input, net, networkBuilder);
+		
+		// prepare component to normalize potential table.
+		NormalizeTableFunction normalizer = new NormalizeTableFunction();
+		
+		// copy the counts to proper place (property of network) and normalize the original potential table (so that they become probabilities)
+		for (Node node : net.getNodes()) {
+			if (node instanceof ProbabilisticNode) {
+				// extract the potential table (it currently stores counts)
+				ProbabilisticTable table = (ProbabilisticTable) ((ProbabilisticNode) node).getProbabilityFunction();
+				
+				// copy count table to proper place
+				PotentialTable countTable = (PotentialTable)table.clone();
+				net.addProperty(DEFAULT_COUNT_TABLE_PREFIX + node.getName(), countTable);
+				
+				// convert original table to table of conditional probabilities (by normalization)
+				normalizer.applyFunction(table);
+			}
+		}
+	}
+	
+
+}
---
+++ UnBBayes/src/main/java/unbbayes/prs/bn/IterativeSecondOrderJunctionTreeAlgorithm.java
@@ -0,0 +1,391 @@
+package unbbayes.prs.bn;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import unbbayes.prs.Node;
+import unbbayes.prs.bn.JunctionTreeAlgorithm;
+import unbbayes.prs.bn.PotentialTable;
+import unbbayes.prs.bn.ProbabilisticNetwork;
+import unbbayes.prs.bn.ProbabilisticNode;
+import cc.mallet.types.Dirichlet;
+
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+import org.apache.log4j.Logger;
+
+import unbbayes.io.CountCompatibleNetIO;
+
+/**
+ * This is an extension to {@link JunctionTreeAlgorithm}
+ * in order to support inference (calculation of joint probability and evidence propagation)
+ * of second order Bayes net (Bayes net with CPTs representing a probability distribution
+ * instead of a single conditional probability).
+ * This version only supports discrete variables, and the higher order CPTs 
+ * are expected to be count tables (parameters of Dirichlet-multinomial distribution)
+ * which should be accessible from {@link unbbayes.prs.bn.ProbabilisticNetwork#getProperty(String)},
+ * with the property name being {@link com.bbn.bn.io.CountCompatibleNetIO#DEFAULT_COUNT_TABLE_PREFIX} + {@link unbbayes.prs.INode#getName()}.
+ * Means and standard deviations of marginals will be stored in {@link Node#setMean(double[])}
+ * and {@link Node#setStandardDeviation(double[])}.
+ * @author Shou Matsumoto
+ * @see com.bbn.bn.io.CountCompatibleNetIO
+ */
+public class IterativeSecondOrderJunctionTreeAlgorithm extends JunctionTreeAlgorithm {
+
+	private int maxIterations = 100;
+	private long maxTimeMillis = 60000;
+	
+	private float initialVirtualCounts = 1;
+	private boolean isToPropagateOnFirstRun = false;
+	
+	private Logger logger = Logger.getLogger(getClass());
+	private boolean isToPropagateOriginalTableAfterSecondOrderPropagation = true;
+	
+	/**
+	 * Default constructor made visible for easy inheritance
+	 */
+	public IterativeSecondOrderJunctionTreeAlgorithm() {
+		super();
+	}
+
+	/**
+	 * Constructor initializing fields
+	 * @param net : second order Bayes net to be compiled
+	 * @param maxIterations : maximum number of samples to generate. The sampling process will stop if this number of iterations
+	 * or maxTimeMillis is reached.
+	 * @param maxTimeMillis : maximum time (milliseconds) to wait before stopping samplings.
+	 * The sampling process will stop if this time is reached, or maxIterations numbers of simulations is reached.
+	 * @see #run()
+	 * @see #setNet(ProbabilisticNetwork)
+	 * @see #setMaxIterations(int)
+	 * @see #setMaxTimeMillis(long)
+	 */
+	public IterativeSecondOrderJunctionTreeAlgorithm(ProbabilisticNetwork net, int maxIterations, long maxTimeMillis) {
+		this();
+		this.setNet(net);
+		this.maxIterations = maxIterations;
+		this.maxTimeMillis = maxTimeMillis;
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see unbbayes.prs.bn.JunctionTreeAlgorithm#getName()
+	 */
+	public String getName() {
+		return getClass().getSimpleName();
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see unbbayes.prs.bn.JunctionTreeAlgorithm#getDescription()
+	 */
+	public String getDescription() {
+		return "Second order Bayes net inference algorithm which samples a CPT and then runs Junction Tree algorithm normally.";
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see unbbayes.prs.bn.JunctionTreeAlgorithm#run()
+	 */
+	public void run() throws IllegalStateException {
+		super.run();
+		if (isToPropagateOnFirstRun) {
+			this.propagate();
+		}
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see unbbayes.prs.bn.JunctionTreeAlgorithm#reset()
+	 */
+	public void reset() {
+		super.reset();
+		if (isToPropagateOnFirstRun) {
+			this.propagate();
+		}
+	}
+	
+	/**
+	 * Performs sampling of first-order CPTs and delegate to superclass.
+	 * This will be iterated {@link #getMaxIterations()} times
+	 * or until {@link #getMaxTimeMillis()} is reached.
+	 * @see unbbayes.prs.bn.JunctionTreeAlgorithm#propagate()
+	 */
+	public void propagate() {
+		
+		// initialize maps of objects that calculate statistics associated with each node
+		Map<ProbabilisticNode, List<DescriptiveStatistics>> mapStatisticsByState = new HashMap<ProbabilisticNode, List<DescriptiveStatistics>>();
+		Map<ProbabilisticNode, List<Dirichlet>> mapDirichletCPT = new HashMap<ProbabilisticNode, List<Dirichlet>>();
+		
+		// backup original cpt values,
+		// initialize statistics of marginals of each node,
+		// and initialize dirichlet distribution for each parents' state in cpt
+		for (Node node : getNet().getNodes()) {
+			
+			if (node instanceof ProbabilisticNode) {
+				
+				// backup cpt
+				((ProbabilisticNode) node).getProbabilityFunction().copyData();
+				
+				// initialize statistics of marginal prob
+				List<DescriptiveStatistics> statisticsByState = new ArrayList<DescriptiveStatistics>(node.getStatesSize());
+				for (int state = 0; state < node.getStatesSize(); state++) {
+					statisticsByState.add(new DescriptiveStatistics());
+				}
+				mapStatisticsByState.put((ProbabilisticNode) node, statisticsByState);
+				
+				// start instantiating dirichlet distribution for cells in cpt
+				
+				// extract count table
+				PotentialTable countTable = (PotentialTable) getNet().getProperty(CountCompatibleNetIO.DEFAULT_COUNT_TABLE_PREFIX + node.getName());
+				if (countTable == null) {
+					getLogger().warn("Count table not found for node " + node.getName() + ". Counts will be ignored for this node.");
+					continue;
+				}
+				
+				// code block following lines below assumes that the variable at index 0 of PotentialTable is the node (owner) itself, 
+				// and other variables are the parents of such node.
+				if (countTable.indexOfVariable(node) != 0) {
+					throw new IllegalArgumentException("Current version assumes that the node which owns a potential/count table is associated with index 0 of such potential/count table,"
+													+ " but index was " + countTable.indexOfVariable(node));
+				}
+				
+				// initialize list to be filled with dirichlet distribution of CPT (separated by each combination of states of parent nodes)
+				List<Dirichlet> dirichletByParentState = new ArrayList<Dirichlet>();
+				
+				// iterate on cells of the count table as cell = base cell + offset (the offset is relative to states of owner node).
+				// Count table contains absolute frequency (as opposed to probabilities -- relative frequencies)
+				// of states of owner node given states of parents
+				for (int baseCellIndex = 0; baseCellIndex < countTable.tableSize(); ) {
+					
+					// extract states of all nodes corresponding to current cell in table
+//					int[] statesAtCurrentCell = countTable.getMultidimensionalCoord(baseCellIndex);
+//					// make sure we are always at a cell corresponding to 1st state (state 0) of owner node
+//					if (statesAtCurrentCell[0] != 0) {	// index 0 of this array is supposedly associated with owner node
+//						throw new RuntimeException("Failed to obtain cell representing the 1st state of the node owning the CPT. This might be a bug.");
+//					}
+					
+					// instantiate dirichlet distribution with the counts we found in current table
+					// parameters of a dirichlet distribution are usually represented with greek alphas in the literature
+					// number of parameters of dirichlet is equal to number of states of the associated random variable
+					double[] alphas = new double[node.getStatesSize()];		
+					for (int stateOffset = 0; stateOffset < alphas.length; stateOffset++) {
+						alphas[stateOffset] = getInitialVirtualCounts() + countTable.getValue(baseCellIndex + stateOffset);
+					}
+					Dirichlet dirichlet = new Dirichlet(alphas);
+					dirichletByParentState.add(dirichlet);
+					
+					// skip to next cell in which the state of CPT owner (current node) is 0 again
+					baseCellIndex += node.getStatesSize();
+				}
+				
+				// keep track of the dirichlet object, because we'll use it during the sampling process
+				mapDirichletCPT.put((ProbabilisticNode) node, dirichletByParentState);
+				
+			}	// end of if (node instanceof ProbabilisticNode)
+			
+		}	// end of iteration for each node
+		
+		getLogger().debug("Starting " + getMaxIterations() 
+				+ " iterations of monte-carlo simulation or for " 
+				+ getMaxTimeMillis() + " milliseconds.");
+		
+		// track start time of sampling process
+		long startTime = System.currentTimeMillis();
+		
+		// start sampling cpts and calculating respective marginals 
+		for (int iteration = 0; iteration < getMaxIterations(); iteration++) {
+			// sample CPTs with dirichlet distribution;
+			for (Node node : getNet().getNodes()) {
+				if (node instanceof ProbabilisticNode) {
+					
+					// extract cpt to overwrite with new sample
+					PotentialTable table = ((ProbabilisticNode) node).getProbabilityFunction();
+					
+					// extract dirichlet objects
+					List<Dirichlet> dirichletByParentState = mapDirichletCPT.get(node);
+					
+					// iterate on number of combination of states of parent nodes 
+					// (this number is equal to number of Dirichlet objects in the list)
+					for (int dirichletListIndex = 0; dirichletListIndex < dirichletByParentState.size(); dirichletListIndex++) {
+						
+						// sample a conditional probability from respective dirichlet dist
+						Dirichlet dirichlet = dirichletByParentState.get(dirichletListIndex);
+						double[] sampleConditionalProb = dirichlet.nextDistribution();
+						
+						// corresponding cell in table is the base cell index (see code below) + offset
+						int baseCellIndex = dirichletListIndex * node.getStatesSize();
+						for (int stateOffset = 0; stateOffset < sampleConditionalProb.length; stateOffset++) {
+							// overwrite cpt by accessing cells by base + offset
+							table.setValue(baseCellIndex + stateOffset, (float)sampleConditionalProb[stateOffset]);
+						}
+						
+					}	// end of iteration for each combination of states of parent nodes
+					
+				}	// end of if (node instanceof ProbabilisticNode)
+				
+			}	// end of iteration for each node
+			
+			// compile/propagate with sampled cpt;
+			super.run();
+			super.propagate();
+			
+			// update statistics of marginals of each node
+			for (Node node : getNet().getNodes()) {
+				if (node instanceof ProbabilisticNode) {
+					// get statistics of marginal prob
+					List<DescriptiveStatistics> statisticsByState = mapStatisticsByState.get(node);
+					// add marginal prob of each state to respective statistics
+					for (int state = 0; state < node.getStatesSize(); state++) {
+						statisticsByState.get(state).addValue(((ProbabilisticNode) node).getMarginalAt(state));
+					}
+				}
+			}
+			
+			// stop sampling if reached time limit
+			if ((System.currentTimeMillis() - startTime) > getMaxTimeMillis()) {
+				getLogger().debug("Halting iteration because max time was reached: " + (System.currentTimeMillis() - startTime));
+				break;
+			}
+		}
+		
+		// restore original cpts;
+		// also update the means and standard deviations of marginals of all nodes;
+		for (Node node : getNet().getNodes()) {
+			if (node instanceof ProbabilisticNode) {
+				// restore cpt
+				((ProbabilisticNode) node).getProbabilityFunction().restoreData();
+				
+				// this is just to make sure array of marginals are allocated
+//				((ProbabilisticNode) node).initMarginalList();	// probably, this is not needed if ran after super.run()
+				
+				// extract statistics of marginal prob
+				List<DescriptiveStatistics> statisticsByState = mapStatisticsByState.get(node);
+				// allocate array of mean and standard deviation of the marginals
+				double[] means = new double[statisticsByState.size()];	// for backward compatibility
+				double[] stdev = new double[statisticsByState.size()];
+				// fill mean and standard deviation of the marginals
+				for (int state = 0; state < statisticsByState.size(); state++) {
+					means[state] = statisticsByState.get(state).getMean();
+					stdev[state] = statisticsByState.get(state).getStandardDeviation();
+					
+					// by default, use mean as the best estimate of marginal probability 
+//					((ProbabilisticNode) node).setMarginalAt(state, (float) means[state]);
+				}
+				// update the values in the nodes
+				node.setMean(means);	// we know this is equal to marginal, but we keep it for backward compatibility
+				node.setStandardDeviation(stdev);
+				
+			}
+		}
+		
+		// run propagation with original table if prompted
+		if (isToPropagateOriginalTableAfterSecondOrderPropagation()) {
+			super.run();
+			super.propagate();
+		}
+	}
+
+	/**
+	 * @return  maximum number of samples to generate. The sampling process will stop if this number of iterations
+	 * or maxTimeMillis is reached.
+	 * @see #propagate()
+	 */
+	public int getMaxIterations() {
+		return maxIterations;
+	}
+
+	/**
+	 * @param maxIterations
+	 * maximum number of samples to generate. The sampling process will stop if this number of iterations
+	 * or maxTimeMillis is reached.
+	 * @see #propagate()
+	 */
+	public void setMaxIterations(int maxIterations) {
+		this.maxIterations = maxIterations;
+	}
+
+	/**
+	 * @return maximum time (milliseconds) to wait before stopping samplings.
+	 * The sampling process will stop if this time is reached, or maxIterations numbers of simulations is reached.
+	 * @see #propagate()
+	 */
+	public long getMaxTimeMillis() {
+		return maxTimeMillis;
+	}
+
+	/**
+	 * @param maxTimeMillis : maximum time (milliseconds) to wait before stopping samplings.
+	 * The sampling process will stop if this time is reached, or maxIterations numbers of simulations is reached.
+	 * @see #propagate()
+	 */
+	public void setMaxTimeMillis(long maxTimeMillis) {
+		this.maxTimeMillis = maxTimeMillis;
+	}
+
+	/**
+	 * @return initial value of parameters of dirichlet distribution.
+	 * The actual parameters of dirichlet distribuion to be used in the sampling process 
+	 * (to sample a "first order" CPT which will be used by the superclass Junction tree algorithm)
+	 * will be this value plus the counts stored at {@link ProbabilisticNetwork#getProperty(String)}
+	 * with name {@link CountCompatibleNetIO#DEFAULT_COUNT_TABLE_PREFIX} + {@link Node#getName()}.
+	 */
+	public float getInitialVirtualCounts() {
+		return initialVirtualCounts;
+	}
+
+	/**
+	 * @param initialVirtualCounts : initial value of parameters of dirichlet distribution.
+	 * The actual parameters of dirichlet distribuion to be used in the sampling process 
+	 * (to sample a "first order" CPT which will be used by the superclass Junction tree algorithm)
+	 * will be this value plus the counts stored at {@link ProbabilisticNetwork#getProperty(String)}
+	 * with name {@link CountCompatibleNetIO#DEFAULT_COUNT_TABLE_PREFIX} + {@link Node#getName()}.
+	 */
+	public void setInitialVirtualCounts(float initialVirtualCounts) {
+		this.initialVirtualCounts = initialVirtualCounts;
+	}
+
+	/**
+	 * @return if true, {@link #run()} and {@link #reset()} will call {@link #propagate()} at the end.
+	 */
+	public boolean isToPropagateOnFirstRun() {
+		return isToPropagateOnFirstRun;
+	}
+
+	/**
+	 * @param isToPropagateOnFirstRun : if true, {@link #run()} and {@link #reset()} will call {@link #propagate()} at the end.
+	 */
+	public void setToPropagateOnFirstRun(boolean isToPropagateOnFirstRun) {
+		this.isToPropagateOnFirstRun = isToPropagateOnFirstRun;
+	}
+
+	public Logger getLogger() {
+		return logger;
+	}
+
+	public void setLogger(Logger logger) {
+		this.logger = logger;
+	}
+
+	/**
+	 * @return
+	 * 		if true, then {@link #propagate()} will run ordinary propagation
+	 * 		with original CPTs at the end.
+	 */
+	public boolean isToPropagateOriginalTableAfterSecondOrderPropagation() {
+		return isToPropagateOriginalTableAfterSecondOrderPropagation;
+	}
+
+	/**
+	 * @param isToPropagateOriginalTableAfterSecondOrderPropagation :
+	 * 		if true, then {@link #propagate()} will run ordinary propagation
+	 * 		with original CPTs at the end.
+	 */
+	public void setToPropagateOriginalTableAfterSecondOrderPropagation(
+			boolean isToPropagateOriginalTableAfterSecondOrderPropagation) {
+		this.isToPropagateOriginalTableAfterSecondOrderPropagation = isToPropagateOriginalTableAfterSecondOrderPropagation;
+	}
+
+}
