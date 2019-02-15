3c994a23a64384990d5e6445ef8474975d93bfd4
cardialfly <cardialfly@edda2279-5339-4143-a38b-a3a7c4ae352d>
2019-02-15 17:01:42 +0000

    Created a tag containing changes related to 2nd order BN (BNs in which CPTs are probability distributions of conditional probabilities). This is used for calculating statistics like standard deviation of marginal probabilities.

--- unbbayes.prs.mebn.softprwol/src/main/java/unbbayes/gui/mebn/extension/kb/triplestore/DatabaseStatusObserver.java
+++
@@ -1,7 +0,0 @@
-package unbbayes.gui.mebn.extension.kb.triplestore;
-
-public interface DatabaseStatusObserver {
-
-	public void update(boolean state); 
-	
-}
--- unbbayes.prs.mebn.softprwol/src/main/java/unbbayes/triplestore/exception/InvalidQuerySintaxException.java
+++
@@ -1,9 +0,0 @@
-package unbbayes.triplestore.exception;
-
-public class InvalidQuerySintaxException extends TriplestoreException{
-
-	public InvalidQuerySintaxException(String msg) {
-		super(msg);
-	}
-	
-}
