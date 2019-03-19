3aaeb0b278a24f18b108e1f164b632438982800c
cardialfly <cardialfly@edda2279-5339-4143-a38b-a3a7c4ae352d>
2019-03-19 00:25:08 +0000

    Included new junit test method for learning from discretized data.

--- unbbayes.learning/src/test/java/unbbayes/learning/TextModeLearningToolkitTest.java
+++ unbbayes.learning/src/test/java/unbbayes/learning/TextModeLearningToolkitTest.java
@@ -49,12 +49,90 @@ public class TextModeLearningToolkitTest {
 		assertNotNull(url);
 		File inputFile = new File(url.toURI());
 		
-		File outputFile = new File("learnedNetWithExtraArcs.net");
-
+		File outputFile = new File("learnedNetWithExtraArcsDataFreq50.net");
+		
 		String prefixNodeFrom = "w1";
 		String prefixNodeTo = "w2";
 //		prefixNodeFrom = "Asia";
 //		prefixNodeTo = "Bronquite";
+		
+		
+		assertTrue(inputFile.exists());
+		
+		// intantiate controller and learning toolkit
+		TextModeConstructionController controller = new TextModeConstructionController(inputFile, null);
+		TextModeLearningToolkit learningKit = new TextModeLearningToolkit(null, controller.getVariables());
+		
+		// initialize data in learning toolkit
+		learningKit.setData(controller.getVariables(), controller.getMatrix(), controller.getVector(), controller.getCaseNumber(), controller.isCompacted());
+		
+		// set structure learning parameter
+		learningKit.setParadigmAlgorithmMetricParam(paradigmAlgorithmMetricParams);
+		
+		// perform structure learning
+		learningKit.buildNet(true);
+		
+		// retrieve the learned model
+		ProbabilisticNetwork learnedNet = learningKit.getLearnedNet();
+		
+		// add extra arcs
+		Set<Edge> newEdges = new HashSet<Edge>();
+		for (Node node : learnedNet.getNodes()) {
+			if (node.getName().startsWith(prefixNodeFrom)) {
+				// obtain respective node which differs only by prefix
+				String targetNodeName = node.getName().replaceFirst(prefixNodeFrom, prefixNodeTo);
+				Node targetNode = learnedNet.getNode(targetNodeName);
+				assertNotNull(targetNodeName, targetNode);
+				Edge edge = new Edge(node, targetNode);
+				learnedNet.addEdge(edge);
+				newEdges.add(edge);
+			}
+		}
+		
+		// perform parameter learning
+		learningKit.runParameterLearning(controller.getVariables(), controller.getMatrix(),
+				controller.getVector(), controller.getCaseNumber());
+		
+		// retrieve the learned model again
+		learnedNet = learningKit.getLearnedNet();
+		assertNotNull(learnedNet);
+		assertTrue(learnedNet.getNodeCount() > 0);		// make sure nodes are present
+		assertTrue(learnedNet.getEdges().size() >= newEdges.size());
+		
+		for (Edge edge : newEdges) {
+			Node originInNet = learnedNet.getNode(edge.getOriginNode().getName());
+			Node destinationInNet = learnedNet.getNode(edge.getDestinationNode().getName());
+			
+			assertTrue(edge.toString(), learnedNet.hasEdge(originInNet, destinationInNet) > 0);
+			
+		}
+		
+		
+		// save network 
+		outputFile.delete();	// delete old file
+		new CountCompatibleNetIO().save(outputFile, learnedNet);
+		assertTrue(outputFile.exists());
+	}
+	
+	/**
+	 * Test method for {@link unbbayes.learning.LearningToolkit}
+	 * with arcs added programatically before parameter learning.
+	 * @throws URISyntaxException 
+	 * @throws IOException 
+	 * @throws InvalidParentException 
+	 */
+	@Test
+	public final void testLearnNetAddArc2() throws URISyntaxException, IOException, InvalidParentException {
+		
+		URL url = getClass().getResource("data_range50.txt");
+		
+		assertNotNull(url);
+		File inputFile = new File(url.toURI());
+		
+		File outputFile = new File("learnedNetWithExtraArcsDataRange50.net");
+
+		String prefixNodeFrom = "w1";
+		String prefixNodeTo = "w2";
 		
 		
 		assertTrue(inputFile.exists());
@@ -113,6 +191,7 @@ public class TextModeLearningToolkitTest {
 		new CountCompatibleNetIO().save(outputFile, learnedNet);
 		assertTrue(outputFile.exists());
 	}
+	
 	/**
 	 * Test method for {@link unbbayes.learning.LearningToolkit}.
 	 * @throws URISyntaxException 
